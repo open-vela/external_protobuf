c705e8d5e29a63ebc921ef0a0222521de7c4dc56
liujinye <liujinye@xiaomi.com>
2008-12-15 10:58:12 +0800

    Remove ugly bootstrapping - we don't need it now.

---
+++ src/ProtoGen.Test/GeneratorTest.cs
@@ -0,0 +1,10 @@
+using Google.ProtocolBuffers.DescriptorProtos;
+using NUnit.Framework;
+using Google.ProtocolBuffers.Descriptors;
+
+namespace Google.ProtocolBuffers.ProtoGen {
+  [TestFixture]
+  public class GeneratorTest {
+
+  }
+}
