91273672f492c66270b419eeafce3e479f43e327
openvela-robot <openvela-robot@xiaomi.com>
2010-11-05 14:49:08 +0800

    Completed abstract lite builder and message

---
+++ src/ProtocolBuffers/GeneratedExtensionLite.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Google.ProtocolBuffers {
+
+  public interface IGeneratedExtensionLite {
+    int Number { get; }
+    object ContainingType { get; }
+    IMessageLite MessageDefaultInstance { get; }
+  }
+}
