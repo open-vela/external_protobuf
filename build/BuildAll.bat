05d8fe08bc34ffa2b7a84396c6a84b9426fd171b
openvela-robot <openvela-robot@xiaomi.com>
2010-11-30 23:54:03 +0800

    Add some explicit casts to eliminate some warnings as described in issue 83 comment 17.

--- build/BuildAll.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:Build /p:BuildConfiguration=Debug
-IF ERRORLEVEL 1 GOTO END
-
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.csproj /m /t:Build /p:BuildConfiguration=Debug_Silverlight2
-
-:END
-PAUSE
