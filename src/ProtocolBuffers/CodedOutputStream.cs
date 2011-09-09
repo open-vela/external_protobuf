e56f423bf5f8f7948597257d5f499eefcd1f9f23
openvela-robot <openvela-robot@xiaomi.com>
2011-09-09 15:53:10 +0800

    Implemented [Obsolete] attribute for deprecated fields

--- src/ProtocolBuffers/CodedOutputStream.cs
+++ src/ProtocolBuffers/CodedOutputStream.cs
@@ -373,7 +373,7 @@ namespace Google.ProtocolBuffers
         public void WriteEnum(int fieldNumber, string fieldName, int value, object rawValue)
         {
             WriteTag(fieldNumber, WireFormat.WireType.Varint);
-            WriteInt32NoTag(value);
+            WriteRawVarint32((uint) value);
         }
 
         public void WriteSFixed32(int fieldNumber, string fieldName, int value)
@@ -648,7 +648,7 @@ namespace Google.ProtocolBuffers
 
         public void WriteEnumNoTag(int value)
         {
-            WriteInt32NoTag(value);
+            WriteRawVarint32((uint) value);
         }
 
         public void WriteSFixed32NoTag(int value)
