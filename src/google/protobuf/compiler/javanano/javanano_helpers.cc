239053346beefbcecdd6b9ecd5d985294a1ce860
openvela-robot <openvela-robot@xiaomi.com>
2013-09-24 17:40:37 +0800

    Implement enum_style=java option.
    This javanano_out command line option creates a container interface
    at the normal place where the enum constants would reside, per enum
    definition. The java_multiple_files flag would now affect the file-
    scope enums with the shells. If the flag is true then file-scope
    container interfaces are created in their own files.
    
    Change-Id: Id52258fcff8d3dee9db8f3d8022147a811bf3565

--- src/google/protobuf/compiler/javanano/javanano_helpers.cc
+++ src/google/protobuf/compiler/javanano/javanano_helpers.cc
@@ -197,12 +197,23 @@ string FileJavaPackage(const Params& params, const FileDescriptor* file) {
 }
 
 bool IsOuterClassNeeded(const Params& params, const FileDescriptor* file) {
-  // Enums and extensions need the outer class as the scope.
-  if (file->enum_type_count() != 0 || file->extension_count() != 0) {
+  // If java_multiple_files is false, the outer class is always needed.
+  if (!params.java_multiple_files(file->name())) {
     return true;
   }
-  // Messages need the outer class only if java_multiple_files is false.
-  return !params.java_multiple_files(file->name());
+
+  // File-scope extensions need the outer class as the scope.
+  if (file->extension_count() != 0) {
+    return true;
+  }
+
+  // If container interfaces are not generated, file-scope enums need the
+  // outer class as the scope.
+  if (file->enum_type_count() != 0 && !params.java_enum_style()) {
+    return true;
+  }
+
+  return false;
 }
 
 string ToJavaName(const Params& params, const string& name, bool is_class,
@@ -228,9 +239,14 @@ string ClassName(const Params& params, const FileDescriptor* descriptor) {
 }
 
 string ClassName(const Params& params, const EnumDescriptor* descriptor) {
-  // An enum's class name is the enclosing message's class name or the outer
-  // class name.
   const Descriptor* parent = descriptor->containing_type();
+  // When using Java enum style, an enum's class name contains the enum name.
+  // Use the standard ToJavaName translation.
+  if (params.java_enum_style()) {
+    return ToJavaName(params, descriptor->name(), true, parent,
+                      descriptor->file());
+  }
+  // Otherwise the enum members are accessed from the enclosing class.
   if (parent != NULL) {
     return ClassName(params, parent);
   } else {
@@ -482,6 +498,13 @@ string GenerateClearBit(int bit_index) {
   string mask = kBitMasks[bit_in_var_index];
   string result = var_name + " = (" + var_name + " & ~" + mask + ")";
   return result;
+}
+
+void SetBitOperationVariables(const string name,
+    int bitIndex, map<string, string>* variables) {
+  (*variables)["get_" + name] = GenerateGetBit(bitIndex);
+  (*variables)["set_" + name] = GenerateSetBit(bitIndex);
+  (*variables)["clear_" + name] = GenerateClearBit(bitIndex);
 }
 
 }  // namespace javanano
