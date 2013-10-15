b919791493d4a895a56a27865c730958212439d9
liujinye <liujinye@xiaomi.com>
2013-10-15 08:38:10 +0800

    am c85806ba: Merge remote-tracking branch \'goog/klp-dev-plus-aosp\' into tomerge
    * commit 'c85806baedfc98ad5759a990b35ea5b5f1ac4edb':

--- src/google/protobuf/compiler/javanano/javanano_primitive_field.cc
+++ src/google/protobuf/compiler/javanano/javanano_primitive_field.cc
@@ -570,15 +570,17 @@ GenerateRepeatedDataSizeCode(io::Printer* printer) const {
 
 void RepeatedPrimitiveFieldGenerator::
 GenerateSerializationCode(io::Printer* printer) const {
-  printer->Print(variables_,
-    "if (this.$name$ != null && this.$name$.length > 0) {\n");
-  printer->Indent();
-
   if (descriptor_->options().packed()) {
-    GenerateRepeatedDataSizeCode(printer);
     printer->Print(variables_,
-      "output.writeRawVarint32($tag$);\n"
-      "output.writeRawVarint32(dataSize);\n"
+      "if (this.$name$.length > 0) {\n");
+    printer->Indent();
+    GenerateRepeatedDataSizeCode(printer);
+    printer->Outdent();
+    printer->Print(variables_,
+      "  output.writeRawVarint32($tag$);\n"
+      "  output.writeRawVarint32(dataSize);\n"
+      "}\n");
+    printer->Print(variables_,
       "for ($type$ element : this.$name$) {\n"
       "  output.write$capitalized_type$NoTag(element);\n"
       "}\n");
@@ -588,15 +590,12 @@ GenerateSerializationCode(io::Printer* printer) const {
       "  output.write$capitalized_type$($number$, element);\n"
       "}\n");
   }
-
-  printer->Outdent();
-  printer->Print("}\n");
 }
 
 void RepeatedPrimitiveFieldGenerator::
 GenerateSerializedSizeCode(io::Printer* printer) const {
   printer->Print(variables_,
-    "if (this.$name$ != null && this.$name$.length > 0) {\n");
+    "if (this.$name$.length > 0) {\n");
   printer->Indent();
 
   GenerateRepeatedDataSizeCode(printer);
