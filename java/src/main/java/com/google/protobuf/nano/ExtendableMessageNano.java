31822ba7bc7d669f0630911e41ba6d57754be02e
openvela-robot <openvela-robot@xiaomi.com>
2014-08-21 17:38:53 +0800

    am 390b6c4b: am 291f0484: (-s ours) Merge commit \'f65ee555\' into fix-merge-conflict
    * commit '390b6c4b7d6982bf2f0904fc2d44b4ebdce29e9e':

--- java/src/main/java/com/google/protobuf/nano/ExtendableMessageNano.java
+++ java/src/main/java/com/google/protobuf/nano/ExtendableMessageNano.java
@@ -31,6 +31,8 @@
 package com.google.protobuf.nano;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Base class of those Protocol Buffer messages that need to store unknown fields,
@@ -42,28 +44,27 @@ public abstract class ExtendableMessageNano<M extends ExtendableMessageNano<M>>
      * A container for fields unknown to the message, including extensions. Extension fields can
      * can be accessed through the {@link #getExtension} and {@link #setExtension} methods.
      */
-    protected FieldArray unknownFieldData;
+    protected List<UnknownFieldData> unknownFieldData;
 
     @Override
     protected int computeSerializedSize() {
         int size = 0;
-        if (unknownFieldData != null) {
-            for (int i = 0; i < unknownFieldData.size(); i++) {
-                FieldData field = unknownFieldData.dataAt(i);
-                size += field.computeSerializedSize();
-            }
+        int unknownFieldCount = unknownFieldData == null ? 0 : unknownFieldData.size();
+        for (int i = 0; i < unknownFieldCount; i++) {
+            UnknownFieldData unknownField = unknownFieldData.get(i);
+            size += CodedOutputByteBufferNano.computeRawVarint32Size(unknownField.tag);
+            size += unknownField.bytes.length;
         }
         return size;
     }
 
     @Override
     public void writeTo(CodedOutputByteBufferNano output) throws IOException {
-        if (unknownFieldData == null) {
-            return;
-        }
-        for (int i = 0; i < unknownFieldData.size(); i++) {
-            FieldData field = unknownFieldData.dataAt(i);
-            field.writeTo(output);
+        int unknownFieldCount = unknownFieldData == null ? 0 : unknownFieldData.size();
+        for (int i = 0; i < unknownFieldCount; i++) {
+            UnknownFieldData unknownField = unknownFieldData.get(i);
+            output.writeRawVarint32(unknownField.tag);
+            output.writeRawBytes(unknownField.bytes);
         }
     }
 
@@ -71,38 +72,14 @@ public abstract class ExtendableMessageNano<M extends ExtendableMessageNano<M>>
      * Gets the value stored in the specified extension of this message.
      */
     public final <T> T getExtension(Extension<M, T> extension) {
-        if (unknownFieldData == null) {
-            return null;
-        }
-        FieldData field = unknownFieldData.get(WireFormatNano.getTagFieldNumber(extension.tag));
-        return field == null ? null : field.getValue(extension);
+        return extension.getValueFrom(unknownFieldData);
     }
 
     /**
      * Sets the value of the specified extension of this message.
      */
     public final <T> M setExtension(Extension<M, T> extension, T value) {
-        int fieldNumber = WireFormatNano.getTagFieldNumber(extension.tag);
-        if (value == null) {
-            if (unknownFieldData != null) {
-                unknownFieldData.remove(fieldNumber);
-                if (unknownFieldData.isEmpty()) {
-                    unknownFieldData = null;
-                }
-            }
-        } else {
-            FieldData field = null;
-            if (unknownFieldData == null) {
-                unknownFieldData = new FieldArray();
-            } else {
-                field = unknownFieldData.get(fieldNumber);
-            }
-            if (field == null) {
-                unknownFieldData.put(fieldNumber, new FieldData(extension, value));
-            } else {
-                field.setValue(extension, value);
-            }
-        }
+        unknownFieldData = extension.setValueTo(value, unknownFieldData);
 
         @SuppressWarnings("unchecked") // Generated code should guarantee type safety
         M typedThis = (M) this;
@@ -129,47 +106,12 @@ public abstract class ExtendableMessageNano<M extends ExtendableMessageNano<M>>
         if (!input.skipField(tag)) {
             return false;  // This wasn't an unknown field, it's an end-group tag.
         }
-        int fieldNumber = WireFormatNano.getTagFieldNumber(tag);
+        if (unknownFieldData == null) {
+            unknownFieldData = new ArrayList<UnknownFieldData>();
+        }
         int endPos = input.getPosition();
         byte[] bytes = input.getData(startPos, endPos - startPos);
-        UnknownFieldData unknownField = new UnknownFieldData(tag, bytes);
-
-        FieldData field = null;
-        if (unknownFieldData == null) {
-            unknownFieldData = new FieldArray();
-        } else {
-            field = unknownFieldData.get(fieldNumber);
-        }
-        if (field == null) {
-            field = new FieldData();
-            unknownFieldData.put(fieldNumber, field);
-        }
-        field.addUnknownField(unknownField);
+        unknownFieldData.add(new UnknownFieldData(tag, bytes));
         return true;
-    }
-
-    /**
-     * Returns whether the stored unknown field data in this message is equivalent to that in the
-     * other message.
-     *
-     * @param other the other message.
-     * @return whether the two sets of unknown field data are equal.
-     */
-    protected final boolean unknownFieldDataEquals(M other) {
-        if (unknownFieldData == null || unknownFieldData.isEmpty()) {
-            return other.unknownFieldData == null || other.unknownFieldData.isEmpty();
-        } else {
-            return unknownFieldData.equals(other.unknownFieldData);
-        }
-    }
-
-    /**
-     * Computes the hashcode representing the unknown field data stored in this message.
-     *
-     * @return the hashcode for the unknown field data.
-     */
-    protected final int unknownFieldDataHashCode() {
-        return (unknownFieldData == null || unknownFieldData.isEmpty()
-                ? 0 : unknownFieldData.hashCode());
     }
 }
