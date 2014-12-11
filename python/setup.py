8342d68bb16899b015059ceda9f541ff28e788b1
liujinye <liujinye@xiaomi.com>
2014-12-11 16:05:06 +0800

    Merge pull request #113 from nsuke/oneof-c++11
    Make C++ code generation of string oneof field compatible with C++11

--- python/setup.py
+++ python/setup.py
@@ -163,7 +163,7 @@ if __name__ == '__main__':
         ))
 
   setup(name = 'protobuf',
-        version = '2.6.2-pre',
+        version = '3.0.0-pre',
         packages = [ 'google' ],
         namespace_packages = [ 'google' ],
         test_suite = 'setup.MakeTestSuite',
