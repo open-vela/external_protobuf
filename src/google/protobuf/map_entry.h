05efa2ae84f3506b617dbafb537e5001bda5552a
openvela-robot <openvela-robot@xiaomi.com>
2014-12-12 10:22:48 +0800

    Returned print_indent and print_outdent back to printservicestub and printserviceclass in python generator

--- src/google/protobuf/map_entry.h
+++ src/google/protobuf/map_entry.h
@@ -43,10 +43,6 @@ class Arena;
 namespace protobuf {
 namespace internal {
 
-// Register all MapEntry default instances so we can delete them in
-// ShutdownProtobufLibrary().
-void LIBPROTOBUF_EXPORT RegisterMapEntryDefaultInstance(MessageLite* default_instance);
-
 // This is the common base class for MapEntry. It is used by MapFieldBase in
 // reflection api, in which the static type of key and value is unknown.
 class LIBPROTOBUF_EXPORT MapEntryBase : public Message {
@@ -84,7 +80,7 @@ class LIBPROTOBUF_EXPORT MapEntryBase : public Message {
 // Moreover, default_enum_value is used to initialize enum field in proto2.
 template <typename Key, typename Value, FieldDescriptor::Type KeyProtoType,
           FieldDescriptor::Type ValueProtoType, int default_enum_value>
-class MapEntry : public MapEntryBase {
+class LIBPROTOBUF_EXPORT MapEntry : public MapEntryBase {
   // Handlers for key/value's proto field type. Used to infer internal layout
   // and provide parsing/serialization support.
   typedef MapProtoTypeHandler<KeyProtoType> KeyProtoHandler;
@@ -321,7 +317,6 @@ class MapEntry : public MapEntryBase {
     entry->reflection_ = reflection;
     entry->default_instance_ = entry;
     entry->InitAsDefaultInstance();
-    RegisterMapEntryDefaultInstance(entry);
     return entry;
   }
 
@@ -363,7 +358,7 @@ class MapEntry : public MapEntryBase {
   template <typename KeyNested, typename ValueNested,
             FieldDescriptor::Type KeyProtoNested,
             FieldDescriptor::Type ValueProtoNested, int default_enum>
-  class MapEntryWrapper
+  class LIBPROTOBUF_EXPORT MapEntryWrapper
       : public MapEntry<KeyNested, ValueNested, KeyProtoNested,
                         ValueProtoNested, default_enum> {
     typedef MapEntry<KeyNested, ValueNested, KeyProtoNested, ValueProtoNested,
@@ -394,7 +389,7 @@ class MapEntry : public MapEntryBase {
   template <typename KeyNested, typename ValueNested,
             FieldDescriptor::Type KeyProtoNested,
             FieldDescriptor::Type ValueProtoNested, int default_enum>
-  class MapEnumEntryWrapper
+  class LIBPROTOBUF_EXPORT MapEnumEntryWrapper
       : public MapEntry<KeyNested, ValueNested, KeyProtoNested,
                         ValueProtoNested, default_enum> {
     typedef MapEntry<KeyNested, ValueNested, KeyProtoNested, ValueProtoNested,
@@ -433,7 +428,7 @@ class MapEntry : public MapEntryBase {
   template <typename K, typename V,
             FieldDescriptor::Type KType,
             FieldDescriptor::Type VType, int default_enum>
-  friend class internal::MapField;
+  friend class LIBPROTOBUF_EXPORT internal::MapField;
   friend class LIBPROTOBUF_EXPORT internal::GeneratedMessageReflection;
 
   GOOGLE_DISALLOW_EVIL_CONSTRUCTORS(MapEntry);
