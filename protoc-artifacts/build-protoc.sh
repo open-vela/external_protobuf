b65c64291ddfcc2169c247bb1757cd622551b568
openvela-robot <openvela-robot@xiaomi.com>
2015-03-31 10:44:05 +0800

    Merge pull request #263 from tamird/gitignore
    Clean up generated files

--- protoc-artifacts/build-protoc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Override the default value set in configure.ac that has '-g' which produces
-# huge binary.
-export CXXFLAGS=-DNDEBUG
-
-cd $(dirname "$0")/.. && ./configure --disable-shared && make &&
-  cd src && (strip protoc || strip protoc.exe)
