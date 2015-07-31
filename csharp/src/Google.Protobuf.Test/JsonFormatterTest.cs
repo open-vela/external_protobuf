b4937fb0b27497efdce6f373e612ff3ae3655509
openvela-robot <openvela-robot@xiaomi.com>
2015-07-31 11:29:07 +0800

    Merge pull request #667 from jskeet/fix-field-orderings
    Fix trivial bug in field orderings.

--- csharp/src/Google.Protobuf.Test/JsonFormatterTest.cs
+++ csharp/src/Google.Protobuf.Test/JsonFormatterTest.cs
@@ -31,16 +31,15 @@
 #endregion
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Google.Protobuf.TestProtos;
 using NUnit.Framework;
-using UnitTest.Issues.TestProtos;
 
 namespace Google.Protobuf
 {
-    /// <summary>
-    /// Tests for the JSON formatter. Note that in these tests, double quotes are replaced with apostrophes
-    /// for the sake of readability (embedding \" everywhere is painful). See the AssertJson method for details.
-    /// </summary>
     public class JsonFormatterTest
     {
         [Test]
@@ -48,16 +47,16 @@ namespace Google.Protobuf
         {
             var formatter = new JsonFormatter(new JsonFormatter.Settings(formatDefaultValues: false));
 
-            AssertJson("{ }", formatter.Format(new ForeignMessage()));
-            AssertJson("{ }", formatter.Format(new TestAllTypes()));
-            AssertJson("{ }", formatter.Format(new TestMap()));
+            Assert.AreEqual("{ }", formatter.Format(new ForeignMessage()));
+            Assert.AreEqual("{ }", formatter.Format(new TestAllTypes()));
+            Assert.AreEqual("{ }", formatter.Format(new TestMap()));
         }
 
         [Test]
         public void DefaultValues_WhenIncluded()
         {
             var formatter = new JsonFormatter(new JsonFormatter.Settings(formatDefaultValues: true));
-            AssertJson("{ 'c': 0 }", formatter.Format(new ForeignMessage()));
+            Assert.AreEqual("{ \"c\": 0 }", formatter.Format(new ForeignMessage()));
         }
 
         [Test]
@@ -90,45 +89,45 @@ namespace Google.Protobuf
             };
             var actualText = JsonFormatter.Default.Format(message);
 
-            // Fields in numeric order
+            // Fields in declaration order, which matches numeric order.
             var expectedText = "{ " +
-                "'singleInt32': 100, " +
-                "'singleInt64': '3210987654321', " +
-                "'singleUint32': 4294967295, " +
-                "'singleUint64': '18446744073709551615', " +
-                "'singleSint32': -456, " +
-                "'singleSint64': '-12345678901235', " +
-                "'singleFixed32': 23, " +
-                "'singleFixed64': '1234567890123', " +
-                "'singleSfixed32': -123, " +
-                "'singleSfixed64': '-12345678901234', " +
-                "'singleFloat': 12.25, " +
-                "'singleDouble': 23.5, " +
-                "'singleBool': true, " +
-                "'singleString': 'test\\twith\\ttabs', " +
-                "'singleBytes': 'AQIDBA==', " +
-                "'singleNestedMessage': { 'bb': 35 }, " +
-                "'singleForeignMessage': { 'c': 10 }, " +
-                "'singleImportMessage': { 'd': 20 }, " +
-                "'singleNestedEnum': 'FOO', " +
-                "'singleForeignEnum': 'FOREIGN_BAR', " +
-                "'singleImportEnum': 'IMPORT_BAZ', " +
-                "'singlePublicImportMessage': { 'e': 54 }" +
+                "\"singleInt32\": 100, " +
+                "\"singleInt64\": \"3210987654321\", " +
+                "\"singleUint32\": 4294967295, " +
+                "\"singleUint64\": \"18446744073709551615\", " +
+                "\"singleSint32\": -456, " +
+                "\"singleSint64\": \"-12345678901235\", " +
+                "\"singleFixed32\": 23, " +
+                "\"singleFixed64\": \"1234567890123\", " +
+                "\"singleSfixed32\": -123, " +
+                "\"singleSfixed64\": \"-12345678901234\", " +
+                "\"singleFloat\": 12.25, " +
+                "\"singleDouble\": 23.5, " +
+                "\"singleBool\": true, " +
+                "\"singleString\": \"test\\twith\\ttabs\", " +
+                "\"singleBytes\": \"AQIDBA==\", " +
+                "\"singleNestedMessage\": { \"bb\": 35 }, " +
+                "\"singleForeignMessage\": { \"c\": 10 }, " +
+                "\"singleImportMessage\": { \"d\": 20 }, " +
+                "\"singleNestedEnum\": \"FOO\", " +
+                "\"singleForeignEnum\": \"FOREIGN_BAR\", " +
+                "\"singleImportEnum\": \"IMPORT_BAZ\", " +
+                "\"singlePublicImportMessage\": { \"e\": 54 }" +
                 " }";
-            AssertJson(expectedText, actualText);
+            Assert.AreEqual(expectedText, actualText);
         }
 
         [Test]
         public void RepeatedField()
         {
-            AssertJson("{ 'repeatedInt32': [ 1, 2, 3, 4, 5 ] }",
+            Assert.AreEqual("{ \"repeatedInt32\": [ 1, 2, 3, 4, 5 ] }",
                 JsonFormatter.Default.Format(new TestAllTypes { RepeatedInt32 = { 1, 2, 3, 4, 5 } }));
         }
 
         [Test]
         public void MapField_StringString()
         {
-            AssertJson("{ 'mapStringString': { 'with spaces': 'bar', 'a': 'b' } }",
+            Assert.AreEqual("{ \"mapStringString\": { \"with spaces\": \"bar\", \"a\": \"b\" } }",
                 JsonFormatter.Default.Format(new TestMap { MapStringString = { { "with spaces", "bar" }, { "a", "b" } } }));
         }
 
@@ -136,7 +135,7 @@ namespace Google.Protobuf
         public void MapField_Int32Int32()
         {
             // The keys are quoted, but the values aren't.
-            AssertJson("{ 'mapInt32Int32': { '0': 1, '2': 3 } }",
+            Assert.AreEqual("{ \"mapInt32Int32\": { \"0\": 1, \"2\": 3 } }",
                 JsonFormatter.Default.Format(new TestMap { MapInt32Int32 = { { 0, 1 }, { 2, 3 } } }));
         }
 
@@ -144,34 +143,34 @@ namespace Google.Protobuf
         public void MapField_BoolBool()
         {
             // The keys are quoted, but the values aren't.
-            AssertJson("{ 'mapBoolBool': { 'false': true, 'true': false } }",
+            Assert.AreEqual("{ \"mapBoolBool\": { \"false\": true, \"true\": false } }",
                 JsonFormatter.Default.Format(new TestMap { MapBoolBool = { { false, true }, { true, false } } }));
         }
 
         [TestCase(1.0, "1")]
-        [TestCase(double.NaN, "'NaN'")]
-        [TestCase(double.PositiveInfinity, "'Infinity'")]
-        [TestCase(double.NegativeInfinity, "'-Infinity'")]
+        [TestCase(double.NaN, "\"NaN\"")]
+        [TestCase(double.PositiveInfinity, "\"Infinity\"")]
+        [TestCase(double.NegativeInfinity, "\"-Infinity\"")]
         public void DoubleRepresentations(double value, string expectedValueText)
         {
             var message = new TestAllTypes { SingleDouble = value };
             string actualText = JsonFormatter.Default.Format(message);
-            string expectedText = "{ 'singleDouble': " + expectedValueText + " }";
-            AssertJson(expectedText, actualText);
+            string expectedText = "{ \"singleDouble\": " + expectedValueText + " }";
+            Assert.AreEqual(expectedText, actualText);
         }
 
         [Test]
         public void UnknownEnumValueOmitted_SingleField()
         {
             var message = new TestAllTypes { SingleForeignEnum = (ForeignEnum) 100 };
-            AssertJson("{ }", JsonFormatter.Default.Format(message));
+            Assert.AreEqual("{ }", JsonFormatter.Default.Format(message));
         }
 
         [Test]
         public void UnknownEnumValueOmitted_RepeatedField()
         {
             var message = new TestAllTypes { RepeatedForeignEnum = { ForeignEnum.FOREIGN_BAZ, (ForeignEnum) 100, ForeignEnum.FOREIGN_FOO } };
-            AssertJson("{ 'repeatedForeignEnum': [ 'FOREIGN_BAZ', 'FOREIGN_FOO' ] }", JsonFormatter.Default.Format(message));
+            Assert.AreEqual("{ \"repeatedForeignEnum\": [ \"FOREIGN_BAZ\", \"FOREIGN_FOO\" ] }", JsonFormatter.Default.Format(message));
         }
 
         [Test]
@@ -179,7 +178,7 @@ namespace Google.Protobuf
         {
             // This matches the C++ behaviour.
             var message = new TestMap { MapInt32Enum = { { 1, MapEnum.MAP_ENUM_FOO }, { 2, (MapEnum) 100 }, { 3, MapEnum.MAP_ENUM_BAR } } };
-            AssertJson("{ 'mapInt32Enum': { '1': 'MAP_ENUM_FOO', '3': 'MAP_ENUM_BAR' } }", JsonFormatter.Default.Format(message));
+            Assert.AreEqual("{ \"mapInt32Enum\": { \"1\": \"MAP_ENUM_FOO\", \"3\": \"MAP_ENUM_BAR\" } }", JsonFormatter.Default.Format(message));
         }
 
         [Test]
@@ -188,14 +187,14 @@ namespace Google.Protobuf
             // *Maybe* we should hold off on writing the "[" until we find that we've got at least one value to write...
             // but this is what happens at the moment, and it doesn't seem too awful.
             var message = new TestAllTypes { RepeatedForeignEnum = { (ForeignEnum) 200, (ForeignEnum) 100 } };
-            AssertJson("{ 'repeatedForeignEnum': [ ] }", JsonFormatter.Default.Format(message));
+            Assert.AreEqual("{ \"repeatedForeignEnum\": [ ] }", JsonFormatter.Default.Format(message));
         }
 
         [Test]
         public void NullValueForMessage()
         {
             var message = new TestMap { MapInt32ForeignMessage = { { 10, null } } };
-            AssertJson("{ 'mapInt32ForeignMessage': { '10': null } }", JsonFormatter.Default.Format(message));
+            Assert.AreEqual("{ \"mapInt32ForeignMessage\": { \"10\": null } }", JsonFormatter.Default.Format(message));
         }
 
         [Test]
@@ -205,14 +204,14 @@ namespace Google.Protobuf
         public void SimpleNonAscii(string text, string encoded)
         {
             var message = new TestAllTypes { SingleString = text };
-            AssertJson("{ 'singleString': '" + encoded + "' }", JsonFormatter.Default.Format(message));
+            Assert.AreEqual("{ \"singleString\": \"" + encoded + "\" }", JsonFormatter.Default.Format(message));
         }
 
         [Test]
         public void SurrogatePairEscaping()
         {
             var message = new TestAllTypes { SingleString = "a\uD801\uDC01b" };
-            AssertJson("{ 'singleString': 'a\\ud801\\udc01b' }", JsonFormatter.Default.Format(message));
+            Assert.AreEqual("{ \"singleString\": \"a\\ud801\\udc01b\" }", JsonFormatter.Default.Format(message));
         }
 
         [Test]
@@ -241,8 +240,8 @@ namespace Google.Protobuf
 
         [Test]
         [TestCase(null, "{ }")]
-        [TestCase("x", "{ 'fooString': 'x' }")]
-        [TestCase("", "{ 'fooString': '' }")]
+        [TestCase("x", "{ \"fooString\": \"x\" }")]
+        [TestCase("", "{ \"fooString\": \"\" }")]
         [TestCase(null, "{ }")]
         public void Oneof(string fooStringValue, string expectedJson)
         {
@@ -254,9 +253,9 @@ namespace Google.Protobuf
 
             // We should get the same result both with and without "format default values".
             var formatter = new JsonFormatter(new JsonFormatter.Settings(false));
-            AssertJson(expectedJson, formatter.Format(message));
+            Assert.AreEqual(expectedJson, formatter.Format(message));
             formatter = new JsonFormatter(new JsonFormatter.Settings(true));
-            AssertJson(expectedJson, formatter.Format(message));
+            Assert.AreEqual(expectedJson, formatter.Format(message));
         }
 
         [Test]
@@ -271,8 +270,8 @@ namespace Google.Protobuf
                 BytesField = ByteString.FromBase64("ABCD"),
                 StringField = ""
             };
-            var expectedJson = "{ 'int64Field': '10', 'int32Field': 0, 'stringField': '', 'bytesField': 'ABCD' }";
-            AssertJson(expectedJson, JsonFormatter.Default.Format(message));
+            var expectedJson = "{ \"int64Field\": \"10\", \"int32Field\": 0, \"stringField\": \"\", \"bytesField\": \"ABCD\" }";
+            Assert.AreEqual(expectedJson, JsonFormatter.Default.Format(message));
         }
 
         [Test]
@@ -284,41 +283,6 @@ namespace Google.Protobuf
             var actualJson = formatter.Format(message);
             Assert.IsTrue(actualJson.Contains("\"int64Field\": null"));
             Assert.IsFalse(actualJson.Contains("\"int32Field\": null"));
-        }
-
-        [Test]
-        public void OutputIsInNumericFieldOrder_NoDefaults()
-        {
-            var formatter = new JsonFormatter(new JsonFormatter.Settings(false));
-            var message = new TestJsonFieldOrdering { PlainString = "p1", PlainInt32 = 2 };
-            AssertJson("{ 'plainString': 'p1', 'plainInt32': 2 }", formatter.Format(message));
-            message = new TestJsonFieldOrdering { O1Int32 = 5, O2String = "o2", PlainInt32 = 10, PlainString = "plain" };
-            AssertJson("{ 'plainString': 'plain', 'o2String': 'o2', 'plainInt32': 10, 'o1Int32': 5 }", formatter.Format(message));
-            message = new TestJsonFieldOrdering { O1String = "", O2Int32 = 0, PlainInt32 = 10, PlainString = "plain" };
-            AssertJson("{ 'plainString': 'plain', 'o1String': '', 'plainInt32': 10, 'o2Int32': 0 }", formatter.Format(message));
-        }
-
-        [Test]
-        public void OutputIsInNumericFieldOrder_WithDefaults()
-        {
-            var formatter = new JsonFormatter(new JsonFormatter.Settings(true));
-            var message = new TestJsonFieldOrdering();
-            AssertJson("{ 'plainString': '', 'plainInt32': 0 }", formatter.Format(message));
-            message = new TestJsonFieldOrdering { O1Int32 = 5, O2String = "o2", PlainInt32 = 10, PlainString = "plain" };
-            AssertJson("{ 'plainString': 'plain', 'o2String': 'o2', 'plainInt32': 10, 'o1Int32': 5 }", formatter.Format(message));
-            message = new TestJsonFieldOrdering { O1String = "", O2Int32 = 0, PlainInt32 = 10, PlainString = "plain" };
-            AssertJson("{ 'plainString': 'plain', 'o1String': '', 'plainInt32': 10, 'o2Int32': 0 }", formatter.Format(message));
-        }
-
-        /// <summary>
-        /// Checks that the actual JSON is the same as the expected JSON - but after replacing
-        /// all apostrophes in the expected JSON with double quotes. This basically makes the tests easier
-        /// to read.
-        /// </summary>
-        private static void AssertJson(string expectedJsonWithApostrophes, string actualJson)
-        {
-            var expectedJson = expectedJsonWithApostrophes.Replace("'", "\"");
-            Assert.AreEqual(expectedJson, actualJson);
         }
     }
 }
