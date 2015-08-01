5fe15e8cc4c829d6d0f6c959a71a070faa4736ba
liujinye <liujinye@xiaomi.com>
2015-08-01 13:30:07 +0800

    Merge pull request #668 from jskeet/json_ordering
    Fix C# JSON field ordering

--- src/google/protobuf/util/internal/utility.cc
+++ src/google/protobuf/util/internal/utility.cc
@@ -30,6 +30,10 @@
 
 #include <google/protobuf/util/internal/utility.h>
 
+#include <cmath>
+#include <algorithm>
+#include <utility>
+
 #include <google/protobuf/stubs/common.h>
 #include <google/protobuf/wrappers.pb.h>
 #include <google/protobuf/descriptor.pb.h>
@@ -297,8 +301,8 @@ bool IsMap(const google::protobuf::Field& field,
 }
 
 string DoubleAsString(double value) {
-  if (google::protobuf::MathLimits<double>::IsPosInf(value)) return "Infinity";
-  if (google::protobuf::MathLimits<double>::IsNegInf(value)) return "-Infinity";
+  if (value == std::numeric_limits<double>::infinity()) return "Infinity";
+  if (value == -std::numeric_limits<double>::infinity()) return "-Infinity";
   if (google::protobuf::MathLimits<double>::IsNaN(value)) return "NaN";
 
   return SimpleDtoa(value);
@@ -316,7 +320,8 @@ bool SafeStrToFloat(StringPiece str, float *value) {
   }
   *value = static_cast<float>(double_value);
 
-  if (google::protobuf::MathLimits<float>::IsInf(*value)) {
+  if ((*value ==  numeric_limits<float>::infinity()) ||
+      (*value == -numeric_limits<float>::infinity())) {
     return false;
   }
   return true;
