9e7cba21b927296a19fbcdfc7a2c9518732fcad2
openvela-robot <openvela-robot@xiaomi.com>
2015-08-08 07:24:28 +0800

    Address review comments.

--- src/google/protobuf/compiler/objectivec/objectivec_file.cc
+++ src/google/protobuf/compiler/objectivec/objectivec_file.cc
@@ -35,6 +35,7 @@
 #include <google/protobuf/compiler/code_generator.h>
 #include <google/protobuf/io/printer.h>
 #include <google/protobuf/io/zero_copy_stream_impl.h>
+#include <google/protobuf/descriptor.pb.h>
 #include <google/protobuf/stubs/stl_util.h>
 #include <google/protobuf/stubs/strutil.h>
 #include <sstream>
