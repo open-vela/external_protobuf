c62091ccd82f99ce62d9c2055a9ddd297273294e
liujinye <liujinye@xiaomi.com>
2015-08-20 20:49:45 +0800

    Fix travis build
    Signed-off-by: Dan O'Reilly <oreilldf@gmail.com>

--- src/google/protobuf/map.h
+++ src/google/protobuf/map.h
@@ -168,10 +168,11 @@ class Map {
       }
     }
 
-#if __cplusplus >= 201103L && !defined(GOOGLE_PROTOBUF_OS_APPLE)
+#if __cplusplus >= 201103L && !defined(GOOGLE_PROTOBUF_OS_APPLE) && \
+    !defined(GOOGLE_PROTOBUF_OS_NACL) && !defined(GOOGLE_PROTOBUF_OS_ANDROID)
     template<class NodeType, class... Args>
     void construct(NodeType* p, Args&&... args) {
-      new (p) NodeType(std::forward<Args>(args)...);
+      new ((void*)p) NodeType(std::forward<Args>(args)...);
     }
 
     template<class NodeType>
--- travis.sh
+++ travis.sh
@@ -8,10 +8,16 @@
 # .travis.yml uses matrix.exclude to block the cases where app-get can't be
 # use to install things.
 
-build_cpp() {
+# For when some other test needs the C++ main build, including protoc and
+# libprotobuf.
+internal_build_cpp() {
   ./autogen.sh
   ./configure
   make -j2
+}
+
+build_cpp() {
+  internal_build_cpp
   make check -j2
   cd conformance && make test_cpp && cd ..
 }
@@ -23,6 +29,11 @@ build_cpp_distcheck() {
 }
 
 build_csharp() {
+  # Just for the conformance tests. We don't currently
+  # need to really build protoc, but it's simplest to keep with the
+  # conventions of the other builds.
+  internal_build_cpp
+
   # Install latest version of Mono
   sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
   echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
@@ -33,6 +44,7 @@ build_csharp() {
 
   (cd csharp/src; mono ../../nuget.exe restore)
   csharp/buildall.sh
+  cd conformance && make test_csharp && cd ..
 }
 
 use_java() {
@@ -62,18 +74,14 @@ use_java() {
 
 build_java() {
   # Java build needs `protoc`.
-  ./autogen.sh
-  ./configure
-  make -j2
+  internal_build_cpp
   cd java && mvn test && cd ..
   cd conformance && make test_java && cd ..
 }
 
 build_javanano() {
   # Java build needs `protoc`.
-  ./autogen.sh
-  ./configure
-  make -j2
+  internal_build_cpp
   cd javanano && mvn test && cd ..
 }
 
@@ -103,45 +111,51 @@ build_javanano_oracle7() {
   build_javanano
 }
 
+internal_install_python_deps() {
+  sudo pip install tox
+  sudo apt-get install -y python-software-properties # for apt-add-repository
+  sudo apt-add-repository -y ppa:fkrull/deadsnakes
+  sudo apt-get update -qq
+  sudo apt-get install -y python2.6 python2.6-dev
+}
+
+
 build_python() {
-  ./autogen.sh
-  ./configure
-  make -j2
+  internal_build_cpp
+  internal_install_python_deps
   cd python
-  python setup.py build
-  python setup.py test
-  python setup.py sdist
-  sudo pip install virtualenv && virtualenv /tmp/protoenv && /tmp/protoenv/bin/pip install dist/*
+  tox -e py26-python,py27-python
   cd ..
 }
 
 build_python_cpp() {
-  ./autogen.sh
-  ./configure
-  make -j2
-  export   LD_LIBRARY_PATH=../src/.libs # for Linux
+  internal_build_cpp
+  internal_install_python_deps
+  export LD_LIBRARY_PATH=../src/.libs # for Linux
   export DYLD_LIBRARY_PATH=../src/.libs # for OS X
   cd python
-  python setup.py build --cpp_implementation
-  python setup.py test --cpp_implementation
-  python setup.py sdist --cpp_implementation
-  sudo pip install virtualenv && virtualenv /tmp/protoenv && /tmp/protoenv/bin/pip install dist/*
+  tox -e py26-cpp,py27-cpp
   cd ..
 }
 
 build_ruby19() {
+  internal_build_cpp  # For conformance tests.
   cd ruby && bash travis-test.sh ruby-1.9 && cd ..
 }
 build_ruby20() {
+  internal_build_cpp  # For conformance tests.
   cd ruby && bash travis-test.sh ruby-2.0 && cd ..
 }
 build_ruby21() {
+  internal_build_cpp  # For conformance tests.
   cd ruby && bash travis-test.sh ruby-2.1 && cd ..
 }
 build_ruby22() {
+  internal_build_cpp  # For conformance tests.
   cd ruby && bash travis-test.sh ruby-2.2 && cd ..
 }
 build_jruby() {
+  internal_build_cpp  # For conformance tests.
   cd ruby && bash travis-test.sh jruby && cd ..
 }
 
