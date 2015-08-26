72b094f8473651a7e0c1285057f60c26bda82b4c
liujinye <liujinye@xiaomi.com>
2015-08-26 13:33:50 +0800

    Merge remote-tracking branch 'origin/master' into beta-1

--- src/google/protobuf/util/internal/utility.cc
+++ src/google/protobuf/util/internal/utility.cc
@@ -30,11 +30,9 @@
 
 #include <google/protobuf/util/internal/utility.h>
 
-#include <cmath>
-#include <algorithm>
-#include <utility>
-
+#include <google/protobuf/stubs/callback.h>
 #include <google/protobuf/stubs/common.h>
+#include <google/protobuf/stubs/logging.h>
 #include <google/protobuf/wrappers.pb.h>
 #include <google/protobuf/descriptor.pb.h>
 #include <google/protobuf/descriptor.h>
@@ -301,15 +299,15 @@ bool IsMap(const google::protobuf::Field& field,
 }
 
 string DoubleAsString(double value) {
-  if (value == std::numeric_limits<double>::infinity()) return "Infinity";
-  if (value == -std::numeric_limits<double>::infinity()) return "-Infinity";
+  if (google::protobuf::MathLimits<double>::IsPosInf(value)) return "Infinity";
+  if (google::protobuf::MathLimits<double>::IsNegInf(value)) return "-Infinity";
   if (google::protobuf::MathLimits<double>::IsNaN(value)) return "NaN";
 
   return SimpleDtoa(value);
 }
 
 string FloatAsString(float value) {
-  if (isfinite(value)) return SimpleFtoa(value);
+  if (google::protobuf::MathLimits<float>::IsFinite(value)) return SimpleFtoa(value);
   return DoubleAsString(value);
 }
 
@@ -320,8 +318,7 @@ bool SafeStrToFloat(StringPiece str, float *value) {
   }
   *value = static_cast<float>(double_value);
 
-  if ((*value ==  numeric_limits<float>::infinity()) ||
-      (*value == -numeric_limits<float>::infinity())) {
+  if (google::protobuf::MathLimits<float>::IsInf(*value)) {
     return false;
   }
   return true;
