7c0cd898d2609b1beab9feeeff6a633d0341e53f
liujinye <liujinye@xiaomi.com>
2015-09-15 15:30:02 +0800

    Rename CMake option BUILD_TESTING to protobuf_BUILD_TESTS

--- python/google/protobuf/pyext/scalar_map_container.h
+++ python/google/protobuf/pyext/scalar_map_container.h
@@ -83,12 +83,7 @@ struct ScalarMapContainer {
   uint64 version;
 };
 
-#if PY_MAJOR_VERSION >= 3
-  extern PyObject *ScalarMapContainer_Type;
-  extern PyType_Spec ScalarMapContainer_Type_spec;
-#else
-  extern PyTypeObject ScalarMapContainer_Type;
-#endif
+extern PyTypeObject ScalarMapContainer_Type;
 extern PyTypeObject ScalarMapIterator_Type;
 
 namespace scalar_map_container {
