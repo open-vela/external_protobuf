f2ccec28ed6be16c5bed1c95b5e7eac2f0318ca1
openvela-robot <openvela-robot@xiaomi.com>
2015-11-03 15:21:30 +0800

    Merge pull request #939 from dougkwan/master
    Add support for POWER Linux

--- csharp/src/Google.Protobuf/Properties/AssemblyInfo.cs
+++ csharp/src/Google.Protobuf/Properties/AssemblyInfo.cs
@@ -46,10 +46,7 @@ using System.Security;
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-#if !NCRUNCH
 [assembly: AllowPartiallyTrustedCallers]
-#endif
 
 #if SIGNED
 [assembly: InternalsVisibleTo("Google.Protobuf.Test, PublicKey=" +
--- protobuf.bzl
+++ protobuf.bzl
@@ -81,7 +81,6 @@ _proto_gen = rule(
         "deps": attr.label_list(providers = ["proto"]),
         "includes": attr.string_list(),
         "protoc": attr.label(
-            cfg = HOST_CFG,
             executable = True,
             single_file = True,
             mandatory = True,
