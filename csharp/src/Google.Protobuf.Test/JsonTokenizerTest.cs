7ab7d5833d7ab7a60a5d0fb55a0e07e8d3956a97
openvela-robot <openvela-robot@xiaomi.com>
2015-11-05 06:11:12 +0800

    Merge pull request #943 from thomasvl/conformance
    Update the Mac build script to include the conformance tests

--- csharp/src/Google.Protobuf.Test/JsonTokenizerTest.cs
+++ csharp/src/Google.Protobuf.Test/JsonTokenizerTest.cs
@@ -223,7 +223,7 @@ namespace Google.Protobuf
             {
                 Assert.IsNotNull(tokenizer.Next());
             }
-            Assert.Throws<InvalidJsonException>(() => tokenizer.Next());
+            Assert.Throws<InvalidProtocolBufferException>(() => tokenizer.Next());
         }
 
         [Test]
@@ -346,7 +346,7 @@ namespace Google.Protobuf
                 }
                 Assert.AreEqual(expectedTokens[i], actualToken);
             }
-            Assert.Throws<InvalidJsonException>(() => tokenizer.Next());
+            Assert.Throws<InvalidProtocolBufferException>(() => tokenizer.Next());
         }
     }
 }
