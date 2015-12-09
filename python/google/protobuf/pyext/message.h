528da5f7c63812f59ed5861646d877d8371bdbb8
liujinye <liujinye@xiaomi.com>
2015-12-09 13:40:38 +0800

    Merge pull request #1033 from c0nk/wip-implicit-cast-fix
    Fix narrowing implicit cast in generated message [NFC]

--- python/google/protobuf/pyext/message.h
+++ python/google/protobuf/pyext/message.h
@@ -52,12 +52,7 @@ class Descriptor;
 class DescriptorPool;
 class MessageFactory;
 
-#ifdef _SHARED_PTR_H
-using std::shared_ptr;
-using std::string;
-#else
 using internal::shared_ptr;
-#endif
 
 namespace python {
 
