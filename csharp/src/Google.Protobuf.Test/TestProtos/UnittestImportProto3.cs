9a0186d97a82f59d90737605af85620babe6606f
openvela-robot <openvela-robot@xiaomi.com>
2015-12-19 10:14:42 +0800

    Initialize singleton instance by GoogleOnceInit()

--- csharp/src/Google.Protobuf.Test/TestProtos/UnittestImportProto3.cs
+++ csharp/src/Google.Protobuf.Test/TestProtos/UnittestImportProto3.cs
@@ -110,7 +110,7 @@ namespace Google.Protobuf.TestProtos {
     }
 
     public override string ToString() {
-      return pb::JsonFormatter.ToDiagnosticString(this);
+      return pb::JsonFormatter.Default.Format(this);
     }
 
     public void WriteTo(pb::CodedOutputStream output) {
