9477e06f9b90254d818d29d47764261154cb9894
liujinye <liujinye@xiaomi.com>
2016-01-12 08:56:17 +0800

    Don't generate Objective C WKT for conformance tests to avoid dupliate defs.

--- objectivec/GPBMessage.m
+++ objectivec/GPBMessage.m
@@ -886,7 +886,6 @@ static GPBUnknownFieldSet *GetOrMakeUnknownFields(GPBMessage *self) {
 - (void)dealloc {
   [self internalClear:NO];
   NSCAssert(!autocreator_, @"Autocreator was not cleared before dealloc.");
-  dispatch_release(readOnlySemaphore_);
   [super dealloc];
 }
 
