50c1c4555c2cb0fb157258b897c0ac870724a583
openvela-robot <openvela-robot@xiaomi.com>
2016-01-15 23:52:33 +0800

    Merge pull request #1146 from haberman/pycppfix
    Fixed Python 3.x C++ build, and updated conformance failure lists.

--- java/core/src/main/java/com/google/protobuf/InvalidProtocolBufferException.java
+++ java/core/src/main/java/com/google/protobuf/InvalidProtocolBufferException.java
@@ -46,10 +46,6 @@ public class InvalidProtocolBufferException extends IOException {
     super(description);
   }
 
-  public InvalidProtocolBufferException(IOException e) {
-    super(e.getMessage(), e);
-  }
-
   /**
    * Attaches an unfinished message to the exception to support best-effort
    * parsing in {@code Parser} interface.
@@ -68,14 +64,6 @@ public class InvalidProtocolBufferException extends IOException {
    */
   public MessageLite getUnfinishedMessage() {
     return unfinishedMessage;
-  }
-
-  /**
-   * Unwraps the underlying {@link IOException} if this exception was caused by an I/O
-   * problem. Otherwise, returns {@code this}.
-   */
-  public IOException unwrapIOException() {
-    return getCause() instanceof IOException ? (IOException) getCause() : this;
   }
 
   static InvalidProtocolBufferException truncatedMessage() {
