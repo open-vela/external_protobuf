0490478f136b9235157bd7ab7b16d2c198bb7efd
openvela-robot <openvela-robot@xiaomi.com>
2016-07-20 10:43:21 +0800

    Merge pull request #1447 from seishun/defaults
    JavaScript: Make implicit defaults consistent with explicit defaults

--- java/core/src/test/java/com/google/protobuf/GeneratedMessageTest.java
+++ java/core/src/test/java/com/google/protobuf/GeneratedMessageTest.java
@@ -972,7 +972,7 @@ public class GeneratedMessageTest extends TestCase {
     TestUtil.MockBuilderParent mockParent = new TestUtil.MockBuilderParent();
 
     TestAllTypes.Builder builder = (TestAllTypes.Builder)
-        ((AbstractMessage) TestAllTypes.getDefaultInstance()).
+        ((GeneratedMessage) TestAllTypes.getDefaultInstance()).
             newBuilderForType(mockParent);
     builder.setOptionalInt32(1);
     builder.setOptionalNestedEnum(TestAllTypes.NestedEnum.BAR);
@@ -1027,7 +1027,7 @@ public class GeneratedMessageTest extends TestCase {
     TestUtil.MockBuilderParent mockParent = new TestUtil.MockBuilderParent();
 
     TestAllExtensions.Builder builder = (TestAllExtensions.Builder)
-        ((AbstractMessage) TestAllExtensions.getDefaultInstance()).
+        ((GeneratedMessage) TestAllExtensions.getDefaultInstance()).
             newBuilderForType(mockParent);
 
     builder.addExtension(UnittestProto.repeatedInt32Extension, 1);
