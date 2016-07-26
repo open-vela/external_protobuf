118d89d21b148cdcdd4045ce9b8ac76fdbca0288
openvela-robot <openvela-robot@xiaomi.com>
2016-07-26 10:01:16 +0800

    Merge pull request #1841 from pherl/3.0.0-GA
    Bump version number for GA

--- conformance/conformance_ruby.rb
+++ conformance/conformance_ruby.rb
@@ -30,7 +30,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-require 'conformance_pb'
+require 'conformance'
 
 $test_count = 0
 $verbose = false
