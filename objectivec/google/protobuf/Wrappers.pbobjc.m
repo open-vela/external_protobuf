b4942053fc161aa2fda43f0c82ad92793afb805e
liujinye <liujinye@xiaomi.com>
2016-09-01 17:12:49 +0800

    Rename UNICODE to protobuf_UNICODE

--- objectivec/google/protobuf/Wrappers.pbobjc.m
+++ objectivec/google/protobuf/Wrappers.pbobjc.m
@@ -27,9 +27,6 @@
 
 @implementation GPBWrappersRoot
 
-// No extensions in the file and no imports, so no need to generate
-// +extensionRegistry.
-
 @end
 
 #pragma mark - GPBWrappersRoot_FileDescriptor
