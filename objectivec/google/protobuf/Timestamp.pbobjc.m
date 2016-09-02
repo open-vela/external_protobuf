decf32ceadf615bcb6ff1189c3d3425795c158cb
liujinye <liujinye@xiaomi.com>
2016-09-02 11:12:25 +0800

    Merge pull request #2026 from pherl/cpjs
    Fix missing import of jspb.Map (#1885)

--- objectivec/google/protobuf/Timestamp.pbobjc.m
+++ objectivec/google/protobuf/Timestamp.pbobjc.m
@@ -27,9 +27,6 @@
 
 @implementation GPBTimestampRoot
 
-// No extensions in the file and no imports, so no need to generate
-// +extensionRegistry.
-
 @end
 
 #pragma mark - GPBTimestampRoot_FileDescriptor
