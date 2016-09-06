6747af32757dc85babf71ff34cce119982cf7edd
openvela-robot <openvela-robot@xiaomi.com>
2016-09-06 14:37:35 +0800

    Update release date in the change log

--- objectivec/google/protobuf/SourceContext.pbobjc.m
+++ objectivec/google/protobuf/SourceContext.pbobjc.m
@@ -27,9 +27,6 @@
 
 @implementation GPBSourceContextRoot
 
-// No extensions in the file and no imports, so no need to generate
-// +extensionRegistry.
-
 @end
 
 #pragma mark - GPBSourceContextRoot_FileDescriptor
