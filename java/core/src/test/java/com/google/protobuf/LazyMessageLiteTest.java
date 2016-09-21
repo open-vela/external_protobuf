b83e4fe4fba261bc4b8ef11764518021796593fb
openvela-robot <openvela-robot@xiaomi.com>
2016-09-21 12:28:15 +0800

    Merge pull request #2149 from khingblue/remove-obsoleted-project
    Remove obsoleted project of j2me

--- java/core/src/test/java/com/google/protobuf/LazyMessageLiteTest.java
+++ java/core/src/test/java/com/google/protobuf/LazyMessageLiteTest.java
@@ -34,8 +34,10 @@ import protobuf_unittest.LazyFieldsLite.LazyExtension;
 import protobuf_unittest.LazyFieldsLite.LazyInnerMessageLite;
 import protobuf_unittest.LazyFieldsLite.LazyMessageLite;
 import protobuf_unittest.LazyFieldsLite.LazyNestedInnerMessageLite;
-import java.util.ArrayList;
+
 import junit.framework.TestCase;
+
+import java.util.ArrayList;
 
 /**
  * Unit test for messages with lazy fields.
