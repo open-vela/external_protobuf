5ac1f46297718a9f2c57d050867e3989938ef1e2
openvela-robot <openvela-robot@xiaomi.com>
2016-09-28 15:34:16 +0800

    Merge pull request #2197 from thomasvl/swift_docs
    Drop the swift docs directory (and content).

--- java/core/src/test/java/com/google/protobuf/LazyStringArrayListTest.java
+++ java/core/src/test/java/com/google/protobuf/LazyStringArrayListTest.java
@@ -32,12 +32,13 @@ package com.google.protobuf;
 
 import static java.util.Arrays.asList;
 
+import junit.framework.TestCase;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.List;
-import junit.framework.TestCase;
 
 /**
  * Tests for {@link LazyStringArrayList}.
--- src/google/protobuf/compiler/cpp/cpp_bootstrap_unittest.cc
+++ src/google/protobuf/compiler/cpp/cpp_bootstrap_unittest.cc
@@ -99,6 +99,7 @@ class MockGeneratorContext : public GeneratorContext {
                           &actual_contents, true));
     EXPECT_TRUE(actual_contents == *expected_contents)
       << physical_filename << " needs to be regenerated.  Please run "
+         "google/protobuf/compiler/release_compiler.sh and "
          "generate_descriptor_proto.sh. Then add this file "
          "to your CL.";
   }
