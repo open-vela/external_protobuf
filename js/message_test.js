b14e2c8df85f8eaf76a814b05e2cee8a64abd9b4
openvela-robot <openvela-robot@xiaomi.com>
2016-11-21 10:32:25 +0800

    Merge pull request #2392 from xfxyjwf/fixdown
    Restore jenkins files.

--- js/message_test.js
+++ js/message_test.js
@@ -1049,7 +1049,13 @@ describe('Message test suite', function() {
     var nested = new proto.jspb.test.Deeply.Nested.Message();
     nested.setCount(5);
     msg.setDeeplyNestedMessage(nested);
-    assertEquals(5, msg.getDeeplyNestedMessage().getCount());
+
+    // After a serialization-deserialization round trip we should get back the
+    // same data we started with.
+    var serialized = msg.toObject();
+    var deserialized =
+        proto.jspb.test.ForeignNestedFieldMessage.fromObject(serialized);
+    assertEquals(5, deserialized.getDeeplyNestedMessage().getCount());
   });
 
 });
