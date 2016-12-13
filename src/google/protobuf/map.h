89d6f61890fd9f11b4f211d305a9f782047ae16b
openvela-robot <openvela-robot@xiaomi.com>
2016-12-13 11:11:18 +0800

    Merge pull request #2484 from ngg/uwp_build
    Add support for Windows ARM builds

--- src/google/protobuf/map.h
+++ src/google/protobuf/map.h
@@ -615,6 +615,7 @@ class Map {
 
 #if __cplusplus >= 201103L && !defined(GOOGLE_PROTOBUF_OS_APPLE) && \
     !defined(GOOGLE_PROTOBUF_OS_NACL) &&                            \
+    !defined(GOOGLE_PROTOBUF_OS_ANDROID) &&                         \
     !defined(GOOGLE_PROTOBUF_OS_EMSCRIPTEN)
     template<class NodeType, class... Args>
     void construct(NodeType* p, Args&&... args) {
