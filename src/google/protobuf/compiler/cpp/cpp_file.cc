36b924e33daa2d7b9bb409f0067238a35ad91124
openvela-robot <openvela-robot@xiaomi.com>
2016-12-22 00:27:02 +0800

    Merge pull request #2525 from camillol/lite
    Eliminate redundant methods in C++ generated code for lite protos

--- src/google/protobuf/compiler/cpp/cpp_file.cc
+++ src/google/protobuf/compiler/cpp/cpp_file.cc
@@ -289,18 +289,6 @@ void FileGenerator::GenerateSource(io::Printer* printer) {
         "classname", message_generators_[i]->classname_);
   }
 
-  for (int i = 0; i < message_generators_.size(); i++) {
-    message_generators_[i]->index_in_metadata_ = i;
-  }
-  for (int i = 0; i < enum_generators_.size(); i++) {
-    enum_generators_[i]->index_in_metadata_ = i;
-  }
-  if (HasGenericServices(file_, options_)) {
-    for (int i = 0; i < service_generators_.size(); i++) {
-      service_generators_[i]->index_in_metadata_ = i;
-    }
-  }
-
   if (HasDescriptorMethods(file_, options_)) {
     printer->Print(
       "\n"
@@ -325,7 +313,16 @@ void FileGenerator::GenerateSource(io::Printer* printer) {
     }
 
     for (int i = 0; i < message_generators_.size(); i++) {
+      message_generators_[i]->index_in_metadata_ = i;
       message_generators_[i]->GenerateDescriptorDeclarations(printer);
+    }
+    for (int i = 0; i < enum_generators_.size(); i++) {
+      enum_generators_[i]->index_in_metadata_ = i;
+    }
+    if (HasGenericServices(file_, options_)) {
+      for (int i = 0; i < service_generators_.size(); i++) {
+        service_generators_[i]->index_in_metadata_ = i;
+      }
     }
 
     printer->Print(
