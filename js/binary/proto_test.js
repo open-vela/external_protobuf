030247826fdd884bd2359120b27955c9fb6ddeca
liujinye <liujinye@xiaomi.com>
2017-03-06 16:48:54 +0800

    Merge pull request #2613 from aausch/fix_memory_leak
     optimization and quick workaround to memory leak

--- js/binary/proto_test.js
+++ js/binary/proto_test.js
@@ -283,7 +283,8 @@ function checkAllFields(original, copy) {
  * @param {!proto.jspb.test.TestExtendable} msg
  */
 function checkExtensions(msg) {
-  assertEquals(0, msg.getExtension(proto.jspb.test.extendOptionalInt32));
+  assertEquals(-42,
+      msg.getExtension(proto.jspb.test.extendOptionalInt32));
   assertEquals(-0x7fffffff00000000,
       msg.getExtension(proto.jspb.test.extendOptionalInt64));
   assertEquals(0x80000000,
@@ -511,7 +512,8 @@ describe('protoBinaryTest', function() {
    * @param {proto.jspb.test.TestExtendable} msg
    */
   function fillExtensions(msg) {
-    msg.setExtension(proto.jspb.test.extendOptionalInt32, 0);
+    msg.setExtension(
+        proto.jspb.test.extendOptionalInt32, -42);
     msg.setExtension(
         proto.jspb.test.extendOptionalInt64, -0x7fffffff00000000);
     msg.setExtension(
