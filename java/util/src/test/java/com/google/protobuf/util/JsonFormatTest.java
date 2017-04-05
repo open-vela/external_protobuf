b2d82e923c4a1af678eb90492a4020a8993399d8
openvela-robot <openvela-robot@xiaomi.com>
2017-04-05 16:34:54 +0800

    Merge pull request #2922 from anandolee/master
    Use PyUnicode_AsEncodedString() instead of PyUnicode_AsEncodedObject()

--- java/util/src/test/java/com/google/protobuf/util/JsonFormatTest.java
+++ java/util/src/test/java/com/google/protobuf/util/JsonFormatTest.java
@@ -1089,7 +1089,7 @@ public class JsonFormatTest extends TestCase {
 
   public void testParserUnexpectedTypeUrl() throws Exception {
     try {
-      Any.Builder builder = Any.newBuilder();
+      TestAllTypes.Builder builder = TestAllTypes.newBuilder();
       mergeFromJson(
           "{\n"
               + "  \"@type\": \"type.googleapis.com/json_test.TestAllTypes\",\n"
