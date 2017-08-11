b75bcef417655f77b7194e8d2c4505d0173e1a4d
openvela-robot <openvela-robot@xiaomi.com>
2017-08-11 15:36:12 +0800

    Merge pull request #3484 from pherl/qualifier
    Make compilers without ref-qualifier support happy.

--- src/google/protobuf/compiler/command_line_interface.cc
+++ src/google/protobuf/compiler/command_line_interface.cc
@@ -108,7 +108,7 @@ namespace compiler {
 #endif
 
 namespace {
-#if defined(_MSC_VER)
+#if defined(_WIN32) && !defined(__CYGWIN__)
 // DO NOT include <io.h>, instead create functions in io_win32.{h,cc} and import
 // them like we do below.
 using google::protobuf::internal::win32::access;
--- src/google/protobuf/compiler/subprocess.cc
+++ src/google/protobuf/compiler/subprocess.cc
@@ -33,7 +33,6 @@
 #include <google/protobuf/compiler/subprocess.h>
 
 #include <algorithm>
-#include <cstring>
 #include <iostream>
 
 #ifndef _WIN32
@@ -51,16 +50,6 @@
 namespace google {
 namespace protobuf {
 namespace compiler {
-
-namespace {
-char* portable_strdup(const char* s) {
-  char* ns = (char*) malloc(strlen(s) + 1);
-  if (ns != NULL) {
-    strcpy(ns, s);
-  }
-  return ns;
-}
-}  // namespace
 
 #ifdef _WIN32
 
@@ -125,7 +114,7 @@ void Subprocess::Start(const string& program, SearchMode search_mode) {
   }
 
   // CreateProcess() mutates its second parameter.  WTF?
-  char* name_copy = portable_strdup(program.c_str());
+  char* name_copy = strdup(program.c_str());
 
   // Create the process.
   PROCESS_INFORMATION process_info;
@@ -309,7 +298,7 @@ void Subprocess::Start(const string& program, SearchMode search_mode) {
   GOOGLE_CHECK(pipe(stdin_pipe) != -1);
   GOOGLE_CHECK(pipe(stdout_pipe) != -1);
 
-  char* argv[2] = { portable_strdup(program.c_str()), NULL };
+  char* argv[2] = { strdup(program.c_str()), NULL };
 
   child_pid_ = fork();
   if (child_pid_ == -1) {
