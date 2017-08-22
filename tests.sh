88959d7ab7c95e25686b1691a7a880b7b3e02a38
liujinye <liujinye@xiaomi.com>
2017-08-22 10:59:39 +0800

    Merge pull request #3540 from drivehappy/3.4.x_clang_cleanup_2
    Merge pull request #3494 from drivehappy/clang_warning_macro

--- tests.sh
+++ tests.sh
@@ -294,7 +294,8 @@ build_python_cpp() {
   cd python
   # Only test Python 2.6/3.x on Linux
   if [ $(uname -s) == "Linux" ]; then
-    envlist=py\{26,27,33,34\}-cpp
+    # py26 is currently disabled due to json_format
+    envlist=py\{27,33,34\}-cpp
   else
     envlist=py27-cpp
   fi
