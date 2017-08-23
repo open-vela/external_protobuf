57bed58d23d4f8d12a01c3fa4944ac9b85b819bf
liujinye <liujinye@xiaomi.com>
2017-08-23 12:33:57 +0800

    Merge pull request #3555 from pherl/fixdist
    Make distcheck aware of test proto files.

--- tests.sh
+++ tests.sh
@@ -61,7 +61,7 @@ build_cpp_distcheck() {
   make dist
 
   # List all files that should be included in the distribution package.
-  git ls-files | grep "^\(java\|python\|objectivec\|csharp\|js\|ruby\|php\|cmake\|examples\)" |\
+  git ls-files | grep "^\(java\|python\|objectivec\|csharp\|js\|ruby\|php\|cmake\|examples\|src/google/protobuf/.*\.proto\)" |\
     grep -v ".gitignore" | grep -v "java/compatibility_tests" |\
     grep -v "cmake/protobuf.*\.pc\.cmake" |\
     grep -v "python/compatibility_tests" | grep -v "csharp/compatibility_tests" > dist.lst
@@ -294,7 +294,8 @@ build_python_cpp() {
   cd python
   # Only test Python 2.6/3.x on Linux
   if [ $(uname -s) == "Linux" ]; then
-    envlist=py\{26,27,33,34\}-cpp
+    # py26 is currently disabled due to json_format
+    envlist=py\{27,33,34\}-cpp
   else
     envlist=py27-cpp
   fi
