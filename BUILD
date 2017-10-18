57cae0beb44dfd53dc1d7596b407a8103d8e8adb
liujinye <liujinye@xiaomi.com>
2017-10-18 15:19:53 +0800

    Merge pull request #3706 from johanbrandhorst/patch-1
    Add GopherJS protobuf and gRPC links

--- BUILD
+++ BUILD
@@ -541,7 +541,6 @@ cc_test(
         "src/google/protobuf/any_test.cc",
         "src/google/protobuf/arena_unittest.cc",
         "src/google/protobuf/arenastring_unittest.cc",
-        "src/google/protobuf/compiler/annotation_test_util.cc",
         "src/google/protobuf/compiler/command_line_interface_unittest.cc",
         "src/google/protobuf/compiler/cpp/cpp_bootstrap_unittest.cc",
         "src/google/protobuf/compiler/cpp/cpp_move_unittest.cc",
--- csharp/src/Google.Protobuf/Reflection/Descriptor.cs
+++ csharp/src/Google.Protobuf/Reflection/Descriptor.cs
@@ -3478,8 +3478,7 @@ namespace Google.Protobuf.Reflection {
         = pb::FieldCodec.ForMessage(7994, global::Google.Protobuf.Reflection.UninterpretedOption.Parser);
     private readonly pbc::RepeatedField<global::Google.Protobuf.Reflection.UninterpretedOption> uninterpretedOption_ = new pbc::RepeatedField<global::Google.Protobuf.Reflection.UninterpretedOption>();
     /// <summary>
-    /// The parser stores options it doesn't recognize here.
-    /// See the documentation for the "Options" section above.
+    /// The parser stores options it doesn't recognize here. See above.
     /// </summary>
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
     public pbc::RepeatedField<global::Google.Protobuf.Reflection.UninterpretedOption> UninterpretedOption {
