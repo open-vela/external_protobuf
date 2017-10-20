cc95c203bcace0b12b754fe5207f34943ab1b843
liujinye <liujinye@xiaomi.com>
2017-10-20 11:56:31 +0800

    Merge pull request #3758 from spinorx/3.4.x
    [v3.4.x branch] Fix iOS cc_library build for protobuf.

--- conformance/conformance_test.cc
+++ conformance/conformance_test.cc
@@ -1995,10 +1995,6 @@ bool ConformanceTestSuite::RunSuite(ConformanceTestRunner* runner,
       R"({"repeatedDuration": ["1.5s", "-1.5s"]})",
       "repeated_duration: {seconds: 1 nanos: 500000000}"
       "repeated_duration: {seconds: -1 nanos: -500000000}");
-  RunValidJsonTest(
-      "DurationNull", REQUIRED,
-      R"({"optionalDuration": null})",
-      "");
 
   ExpectParseFailureForJson(
       "DurationMissingS", REQUIRED,
@@ -2068,10 +2064,6 @@ bool ConformanceTestSuite::RunSuite(ConformanceTestRunner* runner,
       "TimestampWithNegativeOffset", REQUIRED,
       R"({"optionalTimestamp": "1969-12-31T16:00:00-08:00"})",
       "optional_timestamp: {seconds: 0}");
-  RunValidJsonTest(
-      "TimestampNull", REQUIRED,
-      R"({"optionalTimestamp": null})",
-      "");
 
   ExpectParseFailureForJson(
       "TimestampJsonInputTooSmall", REQUIRED,
