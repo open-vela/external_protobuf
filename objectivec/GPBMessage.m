cf2080929c19e5d155cb487425016ed40bf3bfd8
liujinye <liujinye@xiaomi.com>
2017-11-04 09:33:56 +0800

    Fix php well known type conformance tests (#3828) (#3840)
    * Fix php well known type conformance tests
    
    * Properly generate code for test.proto
    
    * Provide GPBMetadata files in c extensions for generated files to import.
    
    * Remove unnecessary test
    
    * Clean up code
    
    * Add declaration for initOnce.
    
    * Refactoring

--- objectivec/GPBMessage.m
+++ objectivec/GPBMessage.m
@@ -2591,14 +2591,13 @@ static void MergeRepeatedNotPackedFieldFromCodedInputStream(
   if (other == self) {
     return YES;
   }
-  if (![other isKindOfClass:[GPBMessage class]]) {
+  if (![other isKindOfClass:[self class]] &&
+      ![self isKindOfClass:[other class]]) {
     return NO;
   }
+
   GPBMessage *otherMsg = other;
   GPBDescriptor *descriptor = [[self class] descriptor];
-  if ([[otherMsg class] descriptor] != descriptor) {
-    return NO;
-  }
   uint8_t *selfStorage = (uint8_t *)messageStorage_;
   uint8_t *otherStorage = (uint8_t *)otherMsg->messageStorage_;
 
