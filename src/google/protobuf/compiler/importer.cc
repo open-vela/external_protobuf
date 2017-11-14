a8358196724f844ed2e1ee2f23789a210bcc6728
openvela-robot <openvela-robot@xiaomi.com>
2017-11-14 23:00:02 +0800

    Travis config cleanups and move ObjC to Xcode 9.1.
    Stop having travis build the test matrix and make it explicit so extra
    settings are only on the test that need the, this helps make the travis
    UI a little cleaner (linux hosted stuff doesn't list an Xcode version),
    as well as avoid unneeded requirements limiting the travis pool something
    can run on.
    
    Small improvements to objectivec/DevTools/full_mac_build.sh to avoid some
    repeated scripting and hopefully make things more reliable work with Xcode
    9.x.
    
    Don't mark the iOS tests as able to fail, hopefully with the new settings
    they are stable and won't give false signals.
    
    Simplify the case for xcode versions and fixup xcode 9.[0-1].

--- src/google/protobuf/compiler/importer.cc
+++ src/google/protobuf/compiler/importer.cc
@@ -65,7 +65,7 @@ namespace google {
 namespace protobuf {
 namespace compiler {
 
-#ifdef _WIN32
+#ifdef _MSC_VER
 // DO NOT include <io.h>, instead create functions in io_win32.{h,cc} and import
 // them like we do below.
 using google::protobuf::internal::win32::access;
