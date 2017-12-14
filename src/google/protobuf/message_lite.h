137e8d775a7cf75203721dcf0ade119a5160505d
openvela-robot <openvela-robot@xiaomi.com>
2017-12-14 13:24:00 +0800

    Fix for php5.5

--- src/google/protobuf/message_lite.h
+++ src/google/protobuf/message_lite.h
@@ -48,8 +48,6 @@
 namespace google {
 namespace protobuf {
 class Arena;
-template <typename T>
-class RepeatedPtrField;
 namespace io {
 class CodedInputStream;
 class CodedOutputStream;
@@ -58,7 +56,6 @@ class ZeroCopyOutputStream;
 }
 namespace internal {
 
-class RepeatedPtrFieldBase;
 class WireFormatLite;
 
 #ifndef SWIG
@@ -382,22 +379,6 @@ class LIBPROTOBUF_EXPORT MessageLite {
 
   virtual uint8* InternalSerializeWithCachedSizesToArray(bool deterministic,
                                                          uint8* target) const;
-
- protected:
-  // CastToBase allows generated code to cast a RepeatedPtrField<T> to
-  // RepeatedPtrFieldBase. We try to restrict access to RepeatedPtrFieldBase
-  // because it is an implementation detail that user code should not access
-  // directly.
-  template <typename T>
-  static ::google::protobuf::internal::RepeatedPtrFieldBase* CastToBase(
-      ::google::protobuf::RepeatedPtrField<T>* repeated) {
-    return repeated;
-  }
-  template <typename T>
-  static const ::google::protobuf::internal::RepeatedPtrFieldBase& CastToBase(
-      const ::google::protobuf::RepeatedPtrField<T>& repeated) {
-    return repeated;
-  }
 
  private:
   // TODO(gerbens) make this a pure abstract function
