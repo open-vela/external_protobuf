6a1ca2f9447578deac91d75494c6adf20860ecd4
openvela-robot <openvela-robot@xiaomi.com>
2018-02-01 13:50:45 +0800

    Merge pull request #4265 from BSBandme/upgrade_benchmark_submodule
    Upgrade benchmark submodule

--- cmake/libprotobuf-lite.cmake
+++ cmake/libprotobuf-lite.cmake
@@ -62,7 +62,5 @@ if(MSVC AND protobuf_BUILD_SHARED_LIBS)
     PRIVATE LIBPROTOBUF_EXPORTS)
 endif()
 set_target_properties(libprotobuf-lite PROPERTIES
-    VERSION ${protobuf_VERSION}
     OUTPUT_NAME ${LIB_PREFIX}protobuf-lite
     DEBUG_POSTFIX "${protobuf_DEBUG_POSTFIX}")
-add_library(protobuf::libprotobuf-lite ALIAS libprotobuf-lite)
