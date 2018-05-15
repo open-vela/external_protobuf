dc07b8034bebe571db43208a1b274d5b372f4d28
openvela-robot <openvela-robot@xiaomi.com>
2018-05-15 09:06:54 +0800

    cmake: Fix DEPENDS of add_custom_command in protobuf_generate

--- kokoro/linux/benchmark/build.sh
+++ kokoro/linux/benchmark/build.sh
@@ -27,8 +27,8 @@ cd $oldpwd
 ./configure CXXFLAGS="-fPIC -O2"
 make -j8
 cd python
-python setup.py build --cpp_implementation
-pip install . --user
+python setup.py -q build --cpp_implementation
+pip install .
 
 
 # build and run Python benchmark
@@ -78,7 +78,6 @@ echo "benchmarking go..."
 make java-benchmark
 echo "benchmarking java..."
 ./java-benchmark -Cresults.file.options.file="tmp/java_result.json" $datasets
-cat $(find /tmp -name "trail-1.log")
 
 # upload result to bq
 make python_add_init
