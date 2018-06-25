41dd2dbb8be0ab2152b6895c3ecf45c55578e464
openvela-robot <openvela-robot@xiaomi.com>
2018-06-25 13:51:42 +0800

    Merge pull request #4172 from OskarKinomoto/master
    Add guards against bswap_XX redefnitions

--- php/tests/compatibility_test.sh
+++ php/tests/compatibility_test.sh
@@ -2,14 +2,12 @@
 
 function use_php() {
   VERSION=$1
-
-  OLD_PATH=$PATH
-  OLD_CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH
-  OLD_C_INCLUDE_PATH=$C_INCLUDE_PATH
-
-  export PATH=/usr/local/php-${VERSION}/bin:$OLD_PATH
-  export CPLUS_INCLUDE_PATH=/usr/local/php-${VERSION}/include/php/main:/usr/local/php-${VERSION}/include/php/:$OLD_CPLUS_INCLUDE_PATH
-  export C_INCLUDE_PATH=/usr/local/php-${VERSION}/include/php/main:/usr/local/php-${VERSION}/include/php/:$OLD_C_INCLUDE_PATH
+  PHP=`which php`
+  PHP_CONFIG=`which php-config`
+  PHPIZE=`which phpize`
+  ln -sfn "/usr/local/php-${VERSION}/bin/php" $PHP
+  ln -sfn "/usr/local/php-${VERSION}/bin/php-config" $PHP_CONFIG
+  ln -sfn "/usr/local/php-${VERSION}/bin/phpize" $PHPIZE
 }
 
 function generate_proto() {
@@ -20,22 +18,7 @@ function generate_proto() {
   mkdir generated
 
   $PROTOC1 --php_out=generated proto/test_include.proto
-  $PROTOC2 --php_out=generated                 \
-    proto/test.proto                           \
-    proto/test_no_namespace.proto              \
-    proto/test_prefix.proto                    \
-    proto/test_php_namespace.proto             \
-    proto/test_empty_php_namespace.proto       \
-    proto/test_reserved_enum_lower.proto       \
-    proto/test_reserved_enum_upper.proto       \
-    proto/test_reserved_enum_value_lower.proto \
-    proto/test_reserved_enum_value_upper.proto \
-    proto/test_reserved_message_lower.proto    \
-    proto/test_reserved_message_upper.proto    \
-    proto/test_service.proto                   \
-    proto/test_service_namespace.proto         \
-    proto/test_descriptors.proto
-
+  $PROTOC2 --php_out=generated proto/test.proto proto/test_no_namespace.proto proto/test_prefix.proto
   pushd ../../src
   $PROTOC2 --php_out=../php/tests/generated -I../php/tests -I. ../php/tests/proto/test_import_descriptor_proto.proto
   popd
@@ -69,9 +52,9 @@ cd $(dirname $0)
 
 # The old version of protobuf that we are testing compatibility against.
 case "$1" in
-  ""|3.5.0)
-    OLD_VERSION=3.5.0
-    OLD_VERSION_PROTOC=http://repo1.maven.org/maven2/com/google/protobuf/protoc/$OLD_VERSION/protoc-$OLD_VERSION-linux-x86_64.exe
+  ""|3.3.0)
+    OLD_VERSION=3.3.0
+    OLD_VERSION_PROTOC=http://repo1.maven.org/maven2/com/google/protobuf/protoc/3.3.0/protoc-3.3.0-linux-x86_64.exe
     ;;
   *)
     echo "[ERROR]: Unknown version number: $1"
@@ -98,7 +81,7 @@ git checkout v$OLD_VERSION
 popd
 
 # Build and copy the new runtime
-use_php 7.1
+use_php 5.5
 pushd ../ext/google/protobuf
 make clean || true
 phpize && ./configure && make
@@ -116,12 +99,12 @@ chmod +x old_protoc
 NEW_PROTOC=`pwd`/../../src/protoc
 OLD_PROTOC=`pwd`/old_protoc
 cd protobuf/php
-composer install
+cp -r /usr/local/vendor-5.5 vendor
+wget https://phar.phpunit.de/phpunit-4.8.0.phar -O /usr/bin/phpunit
 
 # Remove implementation detail tests.
 tests=( array_test.php encode_decode_test.php generated_class_test.php map_field_test.php well_known_test.php )
 sed -i.bak '/php_implementation_test.php/d' phpunit.xml
-sed -i.bak '/generated_phpdoc_test.php/d' phpunit.xml
 for t in "${tests[@]}"
 do
   remove_error_test tests/$t
@@ -135,7 +118,7 @@ cd tests
 generate_proto $OLD_PROTOC $OLD_PROTOC
 ./test.sh
 pushd ..
-./vendor/bin/phpunit
+phpunit
 popd
 
 # Test A.2:
@@ -144,7 +127,7 @@ popd
 generate_proto $NEW_PROTOC $OLD_PROTOC
 ./test.sh
 pushd ..
-./vendor/bin/phpunit
+phpunit
 popd
 
 # Test A.3:
@@ -153,5 +136,5 @@ popd
 generate_proto $OLD_PROTOC $NEW_PROTOC
 ./test.sh
 pushd ..
-./vendor/bin/phpunit
+phpunit
 popd
