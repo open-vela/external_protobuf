184f92b0671199bc59dd3eac1c9271b4cb8ee6c2
openvela-robot <openvela-robot@xiaomi.com>
2018-06-30 14:56:19 +0800

    Remove unclear comment.

--- python/google/protobuf/proto_api.h
+++ python/google/protobuf/proto_api.h
@@ -81,7 +81,7 @@ struct PyProto_API {
 
 inline const char* PyProtoAPICapsuleName() {
   static const char kCapsuleName[] =
-      "google.protobuf.pyext._message.proto_API";
+      "protobuf.python.google.protobuf.cpp._message.proto_API";
   return kCapsuleName;
 }
 
