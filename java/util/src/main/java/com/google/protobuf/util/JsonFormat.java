91e720bc2efed04a467f518cc49f0039a1bb69e4
openvela-robot <openvela-robot@xiaomi.com>
2018-08-09 16:55:29 +0800

    Merge remote-tracking branch 'google/master' into integration_base

--- java/util/src/main/java/com/google/protobuf/util/JsonFormat.java
+++ java/util/src/main/java/com/google/protobuf/util/JsonFormat.java
@@ -611,7 +611,7 @@ public class JsonFormat {
     private final CharSequence blankOrNewLine;
 
     private static class GsonHolder {
-      private static final Gson DEFAULT_GSON = new GsonBuilder().disableHtmlEscaping().create();
+      private static final Gson DEFAULT_GSON = new GsonBuilder().create();
     }
 
     PrinterImpl(
