5a9e286a296de773d5eb210d64614c4c781c70f8
openvela-robot <openvela-robot@xiaomi.com>
2018-08-13 14:41:05 +0800

    Merge pull request #5038 from xfxyjwf/proto2
    Down-integrate proto2 namespace fix.

--- js/binary/reader.js
+++ js/binary/reader.js
@@ -290,7 +290,9 @@ jspb.BinaryReader.prototype.nextField = function() {
       nextWireType != jspb.BinaryConstants.WireType.DELIMITED &&
       nextWireType != jspb.BinaryConstants.WireType.START_GROUP &&
       nextWireType != jspb.BinaryConstants.WireType.END_GROUP) {
-    goog.asserts.fail('Invalid wire type');
+    goog.asserts.fail(
+        'Invalid wire type: %s (at position %s)', nextWireType,
+        this.fieldCursor_);
     this.error_ = true;
     return false;
   }
