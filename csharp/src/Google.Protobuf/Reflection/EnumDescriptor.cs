57dde20598b7447021a4dca2f4a168a23e73d65b
liujinye <liujinye@xiaomi.com>
2019-09-04 14:41:27 +0800

    Sync upb (#6614)
    * Sync upb
    
    https://github.com/protocolbuffers/upb/pull/209
    
    * Update upb
    
    https://github.com/protocolbuffers/upb/pull/210

--- csharp/src/Google.Protobuf/Reflection/EnumDescriptor.cs
+++ csharp/src/Google.Protobuf/Reflection/EnumDescriptor.cs
@@ -128,16 +128,17 @@ namespace Google.Protobuf.Reflection
         /// <summary>
         /// The (possibly empty) set of custom options for this enum.
         /// </summary>
-        [Obsolete("CustomOptions are obsolete. Use GetOption")]
+        //[Obsolete("CustomOptions are obsolete. Use GetOption")]
         public CustomOptions CustomOptions => new CustomOptions(Proto.Options._extensions?.ValuesByNumber);
 
+        /* // uncomment this in the full proto2 support PR
         /// <summary>
         /// Gets a single value enum option for this descriptor
         /// </summary>
         public T GetOption<T>(Extension<EnumOptions, T> extension)
         {
             var value = Proto.Options.GetExtension(extension);
-            return value is IDeepCloneable<T> ? (value as IDeepCloneable<T>).Clone() : value;
+            return value is IDeepCloneable<T> clonable ? clonable.Clone() : value;
         }
 
         /// <summary>
@@ -147,5 +148,6 @@ namespace Google.Protobuf.Reflection
         {
             return Proto.Options.GetExtension(extension).Clone();
         }
+        */
     }
 }
