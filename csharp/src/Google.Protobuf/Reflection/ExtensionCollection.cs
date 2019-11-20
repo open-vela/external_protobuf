3adab9fa62d5511eb6b716ed38737cbbb1f8431c
openvela-robot <openvela-robot@xiaomi.com>
2019-11-20 10:55:48 +0800

    Fix formatting

--- csharp/src/Google.Protobuf/Reflection/ExtensionCollection.cs
+++ csharp/src/Google.Protobuf/Reflection/ExtensionCollection.cs
@@ -107,13 +107,13 @@ namespace Google.Protobuf.Reflection
             {
                 descriptor.CrossLink();
 
-                IList<FieldDescriptor> _;
-                if (!declarationOrder.TryGetValue(descriptor.ExtendeeType, out _))
+                IList<FieldDescriptor> list;
+                if (!declarationOrder.TryGetValue(descriptor.ExtendeeType, out list))
                 {
-                    declarationOrder.Add(descriptor.ExtendeeType, new List<FieldDescriptor>());
+                    declarationOrder.Add(descriptor.ExtendeeType, list = new List<FieldDescriptor>());
                 }
 
-                declarationOrder[descriptor.ExtendeeType].Add(descriptor);
+                list.Add(descriptor);
             }
 
             extensionsByTypeInDeclarationOrder = declarationOrder
