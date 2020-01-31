f5d55312803a033326ecbdd1e8bdefd353986485
openvela-robot <openvela-robot@xiaomi.com>
2020-01-31 12:43:25 +0800

    deps: update errorprone to 2.3.4 (#7125) (#7157)

--- objectivec/google/protobuf/Duration.pbobjc.m
+++ objectivec/google/protobuf/Duration.pbobjc.m
@@ -68,7 +68,7 @@ typedef struct GPBDuration__storage_ {
     static GPBMessageFieldDescription fields[] = {
       {
         .name = "seconds",
-        .dataTypeSpecific.clazz = Nil,
+        .dataTypeSpecific.className = NULL,
         .number = GPBDuration_FieldNumber_Seconds,
         .hasIndex = 0,
         .offset = (uint32_t)offsetof(GPBDuration__storage_, seconds),
@@ -77,7 +77,7 @@ typedef struct GPBDuration__storage_ {
       },
       {
         .name = "nanos",
-        .dataTypeSpecific.clazz = Nil,
+        .dataTypeSpecific.className = NULL,
         .number = GPBDuration_FieldNumber_Nanos,
         .hasIndex = 1,
         .offset = (uint32_t)offsetof(GPBDuration__storage_, nanos),
@@ -92,7 +92,7 @@ typedef struct GPBDuration__storage_ {
                                         fields:fields
                                     fieldCount:(uint32_t)(sizeof(fields) / sizeof(GPBMessageFieldDescription))
                                    storageSize:sizeof(GPBDuration__storage_)
-                                         flags:GPBDescriptorInitializationFlag_UsesClassRefs];
+                                         flags:GPBDescriptorInitializationFlag_None];
     #if defined(DEBUG) && DEBUG
       NSAssert(descriptor == nil, @"Startup recursed!");
     #endif  // DEBUG
--- tests.sh
+++ tests.sh
@@ -195,7 +195,7 @@ use_java() {
   $MVN -version
 }
 
-# --batch-mode suppresses download progress output that spams the logs.
+# --batch-mode supresses download progress output that spams the logs.
 MVN="mvn --batch-mode"
 
 build_java() {
@@ -233,7 +233,7 @@ build_java_oracle7() {
 build_java_compatibility() {
   use_java jdk7
   internal_build_cpp
-  # Use the unit-tests extracted from 2.5.0 to test the compatibility between
+  # Use the unit-tests extraced from 2.5.0 to test the compatibilty between
   # 3.0.0-beta-4 and the current version.
   cd java/compatibility_tests/v2.5.0
   ./test.sh 3.0.0-beta-4
@@ -251,6 +251,16 @@ build_java_linkage_monitor() {
   # Linkage Monitor uses $HOME/.m2 local repository
   MVN="mvn -e -B -Dhttps.protocols=TLSv1.2"
   cd java
+  # Sets java artifact version with SNAPSHOT, as Linkage Monitor looks for SNAPSHOT versions.
+  # Example: "3.9.0" (without 'rc')
+  VERSION=`grep '<version>' pom.xml |head -1 |perl -nle 'print $1 if m/<version>(\d+\.\d+.\d+)/'`
+  cd bom
+  # This local installation avoids the problem caused by a new version not yet in Maven Central
+  # https://github.com/protocolbuffers/protobuf/issues/6627
+  $MVN install
+  $MVN versions:set -DnewVersion=${VERSION}-SNAPSHOT
+  cd ..
+  $MVN versions:set -DnewVersion=${VERSION}-SNAPSHOT
   # Installs the snapshot version locally
   $MVN install -Dmaven.test.skip=true
 
@@ -408,7 +418,7 @@ build_python38_cpp() {
 
 build_python_compatibility() {
   internal_build_cpp
-  # Use the unit-tests extracted from 2.5.0 to test the compatibility.
+  # Use the unit-tests extraced from 2.5.0 to test the compatibilty.
   cd python/compatibility_tests/v2.5.0
   # Test between 2.5.0 and the current version.
   ./test.sh 2.5.0
