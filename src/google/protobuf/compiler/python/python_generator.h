3ceee53ce9c1ae31d3b8e79bebe22ceb67a15faa
liujinye <liujinye@xiaomi.com>
2020-04-13 17:34:22 +0800

    Merge pull request #7360 from jtattermusch/refactor_googlebenchmark
    Refactor some Protobuf C# microbenchmarks

--- src/google/protobuf/compiler/python/python_generator.h
+++ src/google/protobuf/compiler/python/python_generator.h
@@ -69,9 +69,10 @@ class PROTOC_EXPORT Generator : public CodeGenerator {
   virtual ~Generator();
 
   // CodeGenerator methods.
-  bool Generate(const FileDescriptor* file, const std::string& parameter,
-                GeneratorContext* generator_context,
-                std::string* error) const override;
+  virtual bool Generate(const FileDescriptor* file,
+                        const std::string& parameter,
+                        GeneratorContext* generator_context,
+                        std::string* error) const override;
 
   uint64 GetSupportedFeatures() const override;
 
