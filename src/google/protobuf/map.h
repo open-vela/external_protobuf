906e18c28eed06b8a7dd26e3d3818cd881ef8eeb
openvela-robot <openvela-robot@xiaomi.com>
2020-05-05 16:57:52 +0800

    Merge pull request #7351 from jtattermusch/protobuf_csharp_new_parsing
    New Span-based C# parsing logic

--- src/google/protobuf/map.h
+++ src/google/protobuf/map.h
@@ -37,7 +37,6 @@
 #ifndef GOOGLE_PROTOBUF_MAP_H__
 #define GOOGLE_PROTOBUF_MAP_H__
 
-#include <functional>
 #include <initializer_list>
 #include <iterator>
 #include <limits>  // To support Visual Studio 2008
@@ -269,7 +268,7 @@ class Map {
   using const_reference = const value_type&;
 
   using size_type = size_t;
-  using hasher = std::hash<Key>;
+  using hasher = hash<Key>;
 
   Map() : arena_(nullptr), default_enum_value_(0) { Init(); }
   explicit Map(Arena* arena) : arena_(arena), default_enum_value_(0) { Init(); }
