5a19c8c3145f8989d3ee5a77ef9529e72149fb7e
liujinye <liujinye@xiaomi.com>
2020-06-03 11:43:59 +0800

    Fixed all UBSan issues and added UBSan CI checks.

---
+++ kokoro/ubuntu/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Install the latest version of Bazel.
+use_bazel.sh latest
+
+# Verify/query CMake
+echo PATH=$PATH
+ls -l `which cmake`
+cmake --version
+
+# Log the bazel path and version.
+which bazel
+bazel version
+
+cd $(dirname $0)/../..
+bazel test --test_output=errors :all
+
+if [[ $(uname) = "Linux" ]]; then
+  # Verify the ASAN build.  Have to exclude test_conformance_upb as protobuf
+  # currently leaks memory in the conformance test runner.
+  bazel test --copt=-fsanitize=address --linkopt=-fsanitize=address --test_output=errors -- :all -:test_conformance_upb
+
+  # Verify the UBSan build. Have to exclude Lua as the version we are using
+  # fails some UBSan tests.
+  CC=clang CXX=clang++ bazel test -c dbg --copt=-fsanitize=undefined --copt=-fno-sanitize=function,vptr --linkopt=-fsanitize=undefined --action_env=UBSAN_OPTIONS=halt_on_error=1:print_stacktrace=1 -- :all -:test_lua
+fi
