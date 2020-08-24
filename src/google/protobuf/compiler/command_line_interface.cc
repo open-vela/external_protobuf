b9110d45eea4fd9b09af1dca50e5d8385b29a9e4
liujinye <liujinye@xiaomi.com>
2020-08-24 15:48:43 +0800

    Sync from Piper @328224070
    PROTOBUF_SYNC_PIPER

--- src/google/protobuf/compiler/command_line_interface.cc
+++ src/google/protobuf/compiler/command_line_interface.cc
@@ -1842,7 +1842,7 @@ CommandLineInterface::InterpretArgument(const std::string& name,
       std::cout << version_info_ << std::endl;
     }
     std::cout << "libprotoc " << internal::VersionString(PROTOBUF_VERSION)
-              << PROTOBUF_VERSION_SUFFIX << std::endl;
+              << std::endl;
     return PARSE_ARGUMENT_DONE_AND_EXIT;  // Exit without running compiler.
 
   } else if (name == "--disallow_services") {
