28d1c19b96a38ce40f580027c737f6e22eb754fb
openvela-robot <openvela-robot@xiaomi.com>
2020-10-15 20:07:42 +0800

    Merge branch 'fastest-table' into fast-table

--- upb/decode.int.h
+++ upb/decode.int.h
@@ -25,8 +25,18 @@ typedef struct upb_decstate {
 } upb_decstate;
 
 const char *fastdecode_dispatch(upb_decstate *d, const char *ptr, upb_msg *msg,
-                                const upb_msglayout *table, uint64_t hasbits);
+                                intptr_t table, uint64_t hasbits);
 const char *fastdecode_err(upb_decstate *d);
+
+/* x86-64 pointers always have the high 16 bits matching. So we can shift
+ * left 8 and right 8 without loss of information. */
+UPB_INLINE intptr_t decode_totable(const upb_msglayout *tablep) {
+  return ((intptr_t)tablep << 8) | tablep->table_mask;
+}
+
+UPB_INLINE const upb_msglayout *decode_totablep(intptr_t table) {
+  return (void*)(table >> 8);
+}
 
 #include "upb/port_undef.inc"
 
