5f6a6d341d12a6fe1954baa75013ca4ca0df90fb
liujinye <liujinye@xiaomi.com>
2020-10-15 19:58:07 +0800

    Added a bunch of comments for readability.

--- upb/def.c
+++ upb/def.c
@@ -940,8 +940,9 @@ static bool make_layout(const upb_symtab *symtab, const upb_msgdef *m) {
   const upb_msglayout **submsgs;
   upb_msglayout_field *fields;
   upb_alloc *alloc = upb_arena_alloc(symtab->arena);
+  size_t i;
 
-  memset(l, 0, sizeof(*l) + sizeof(_upb_fasttable_entry));
+  memset(l, 0, sizeof(*l));
 
   fields = upb_malloc(alloc, upb_msgdef_numfields(m) * sizeof(*fields));
   submsgs = upb_malloc(alloc, submsg_count * sizeof(*submsgs));
@@ -955,10 +956,12 @@ static bool make_layout(const upb_symtab *symtab, const upb_msgdef *m) {
   l->field_count = upb_msgdef_numfields(m);
   l->fields = fields;
   l->submsgs = submsgs;
-  l->table_mask = 0;
 
-  l->fasttable[0].field_parser = &fastdecode_generic;
-  l->fasttable[0].field_data = 0;
+  /* TODO(haberman): initialize fast tables so that reflection-based parsing
+   * can get the same speeds as linked-in types. */
+  for (i = 0; i < 32; i++) {
+    l->fasttable[i].field_parser = &fastdecode_generic;
+  }
 
   if (upb_msgdef_mapentry(m)) {
     /* TODO(haberman): refactor this method so this special case is more
@@ -1721,8 +1724,7 @@ static bool create_msgdef(symtab_addctx *ctx, const char *prefix,
     ctx->layouts++;
   } else {
     /* Allocate now (to allow cross-linking), populate later. */
-    m->layout = upb_malloc(ctx->alloc,
-                           sizeof(*m->layout) + sizeof(_upb_fasttable_entry));
+    m->layout = upb_malloc(ctx->alloc, sizeof(*m->layout));
   }
 
   oneofs = google_protobuf_DescriptorProto_oneof_decl(msg_proto, &n);
