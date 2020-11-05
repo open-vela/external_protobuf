d3d6e7d835725951e23e7a7f3fe083301481bb3b
liujinye <liujinye@xiaomi.com>
2020-11-05 10:23:03 +0800

    Updated Kokoro build script.

--- kokoro/ubuntu/build.sh
+++ kokoro/ubuntu/build.sh
@@ -20,8 +20,22 @@ cd $(dirname $0)/../..
 
 if which gcc; then
   gcc --version
+  bazel test --test_output=errors ...
+  # The checked-in code is with fasttable not enabled.
+  bazel test --test_output=errors ... --//:fasttable_enabled=true -- -cmake:test_generated_files
+fi
+
+if which clang; then
+  CC=clang bazel test --test_output=errors ...
+  # The checked-in code is with fasttable not enabled.
+  CC=clang bazel test --test_output=errors ... --//:fasttable_enabled=true -- -cmake:test_generated_files
+fi
+
+if which gcc; then
+  gcc --version
   CC=gcc bazel test --test_output=errors ...
   CC=gcc bazel test -c opt --test_output=errors ...
+  CC=gcc bazel test --test_output=errors ... --//:fasttable_enabled=true -- -cmake:test_generated_files
   # TODO: work through these errors and enable this.
   # if gcc -fanalyzer -x c /dev/null -c -o /dev/null; then
   #   CC=gcc bazel test --copt=-fanalyzer --test_output=errors ...
@@ -31,6 +45,7 @@ fi
 if which clang; then
   CC=clang bazel test --test_output=errors ...
   CC=clang bazel test --test_output=errors -c opt ...
+  CC=clang bazel test --test_output=errors ... --//:fasttable_enabled=true -- -cmake:test_generated_files
 
   if [[ $(uname) = "Linux" ]]; then
     CC=clang bazel test --test_output=errors --config=m32 ...
