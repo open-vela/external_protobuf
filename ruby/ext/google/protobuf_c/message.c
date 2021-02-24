ccf7ce2d860d3aab18f6ba902d92b49e6bbe297c
liujinye <liujinye@xiaomi.com>
2021-02-24 16:46:27 +0800

    Merge pull request #8338 from acozzette/3.15.x-202102231541
    Merge 3.15.x into master

--- ruby/ext/google/protobuf_c/message.c
+++ ruby/ext/google/protobuf_c/message.c
@@ -105,7 +105,7 @@ void Message_InitPtr(VALUE self_, upb_msg *msg, VALUE arena) {
   Message* self = ruby_to_Message(self_);
   self->msg = msg;
   self->arena = arena;
-  ObjectCache_Add(msg, self_);
+  ObjectCache_Add(msg, self_, Arena_get(arena));
 }
 
 VALUE Message_GetArena(VALUE msg_rb) {
@@ -855,10 +855,8 @@ static VALUE Message_to_h(VALUE _self) {
  */
 static VALUE Message_freeze(VALUE _self) {
   Message* self = ruby_to_Message(_self);
-  if (!RB_OBJ_FROZEN(_self)) {
-    Arena_Pin(self->arena, _self);
-    RB_OBJ_FREEZE(_self);
-  }
+  ObjectCache_Pin(self->msg, _self, Arena_get(self->arena));
+  RB_OBJ_FREEZE(_self);
   return _self;
 }
 
