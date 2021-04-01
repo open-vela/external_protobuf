6c4abdd45ecdc40d29f95cfe59e1d2188f8ecc34
liujinye <liujinye@xiaomi.com>
2021-04-01 12:15:56 +0800

    Remove references to stale benchmark data sources.

--- ruby/ext/google/protobuf_c/protobuf.h
+++ ruby/ext/google/protobuf_c/protobuf.h
@@ -106,8 +106,6 @@ extern VALUE cTypeError;
 #define PBRUBY_ASSERT(expr) assert(expr)
 #endif
 
-#define PBRUBY_MAX(x, y) (((x) > (y)) ? (x) : (y))
-
 #define UPB_UNUSED(var) (void)var
 
 #endif  // __GOOGLE_PROTOBUF_RUBY_PROTOBUF_H__
