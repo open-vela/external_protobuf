9a3310d1b19d3049a876d8065661a5b474e25984
liujinye <liujinye@xiaomi.com>
2021-04-02 14:55:02 +0800

    Merge branch 'master' into 3.15.x

--- src/google/protobuf/compiler/java/java_map_field.h
+++ src/google/protobuf/compiler/java/java_map_field.h
@@ -62,7 +62,6 @@ class ImmutableMapFieldGenerator : public ImmutableFieldGenerator {
   void GenerateFieldBuilderInitializationCode(io::Printer* printer) const;
   void GenerateEqualsCode(io::Printer* printer) const;
   void GenerateHashCode(io::Printer* printer) const;
-  void GenerateKotlinDslMembers(io::Printer* printer) const;
 
   std::string GetBoxedType() const;
 
