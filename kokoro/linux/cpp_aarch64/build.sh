4fc031a1b892e30eff7281336637f0b5caf3aeb6
openvela-robot <openvela-robot@xiaomi.com>
2021-04-29 18:20:55 +0800

    Merge pull request #8391 from jtattermusch/cpp_aarch64_emulated_tests
    run basic C++ aarch64 tests under qemu emulator

--- kokoro/linux/cpp_aarch64/build.sh
+++ kokoro/linux/cpp_aarch64/build.sh
@@ -8,9 +8,4 @@ set -ex
 # Change to repo root
 cd $(dirname $0)/../../..
 
-# Initialize any submodules.
-git submodule update --init --recursive
-
-kokoro/linux/aarch64/qemu_helpers/prepare_qemu.sh
-
-kokoro/linux/aarch64/test_javascript_aarch64.sh
+kokoro/linux/aarch64/test_cpp_aarch64.sh
