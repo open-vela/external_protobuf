6c7a18117edb01975b2ebde05065b1914d5e62f4
openvela-robot <openvela-robot@xiaomi.com>
2021-05-14 17:50:31 +0800

    Sync from Piper @373820256
    PROTOBUF_SYNC_PIPER

--- java/kotlin/src/test/kotlin/com/google/protobuf/Proto2Test.kt
+++ java/kotlin/src/test/kotlin/com/google/protobuf/Proto2Test.kt
@@ -1,15 +1,45 @@
+// Protocol Buffers - Google's data interchange format
+// Copyright 2008 Google Inc.  All rights reserved.
+// https://developers.google.com/protocol-buffers/
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following disclaimer
+// in the documentation and/or other materials provided with the
+// distribution.
+//     * Neither the name of Google Inc. nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 package com.google.protobuf.kotlin
 
 import com.google.common.truth.Truth.assertThat
 import com.google.protobuf.TestUtil
 import com.google.protobuf.TestUtil.toBytes
-import evil_names_proto2.EvilNamesProto2OuterClass.EvilNamesProto2
-import evil_names_proto2.EvilNamesProto2OuterClass.HardKeywordsAllTypes
-import evil_names_proto2.EvilNamesProto2OuterClass.Interface
-import evil_names_proto2.HardKeywordsAllTypesKt
-import evil_names_proto2.evilNamesProto2
-import evil_names_proto2.hardKeywordsAllTypes
-import evil_names_proto2.interface_
+import com.google.protobuf.kotlin.generator.EvilNamesProto2OuterClass.EvilNamesProto2
+import com.google.protobuf.kotlin.generator.EvilNamesProto2OuterClass.HardKeywordsAllTypes
+import com.google.protobuf.kotlin.generator.EvilNamesProto2OuterClass.Interface
+import com.google.protobuf.kotlin.generator.HardKeywordsAllTypesKt
+import com.google.protobuf.kotlin.generator.evilNamesProto2
+import com.google.protobuf.kotlin.generator.hardKeywordsAllTypes
+import com.google.protobuf.kotlin.generator.interface_
 import com.google.protobuf.test.UnittestImport.ImportEnum
 import com.google.protobuf.test.UnittestImport.ImportMessage
 import com.google.protobuf.test.UnittestImportPublic.PublicImportMessage
@@ -465,12 +495,12 @@ class Proto2Test {
         this[UnittestProto.repeatedBytesExtension] += toBytes("316")
         this[UnittestProto.repeatedGroupExtension] += repeatedGroupExtension { a = 317 }
         this[UnittestProto.repeatedNestedMessageExtension] +=
-	  TestAllTypesKt.nestedMessage { bb = 318 }
+          TestAllTypesKt.nestedMessage { bb = 318 }
         this[UnittestProto.repeatedForeignMessageExtension] += foreignMessage { c = 319 }
         this[UnittestProto.repeatedImportMessageExtension] +=
           ImportMessage.newBuilder().setD(320).build()
         this[UnittestProto.repeatedLazyMessageExtension] +=
-	  TestAllTypesKt.nestedMessage { bb = 327 }
+          TestAllTypesKt.nestedMessage { bb = 327 }
         this[UnittestProto.repeatedNestedEnumExtension] += NestedEnum.BAZ
         this[UnittestProto.repeatedForeignEnumExtension] += ForeignEnum.FOREIGN_BAZ
         this[UnittestProto.repeatedImportEnumExtension] += ImportEnum.IMPORT_BAZ
