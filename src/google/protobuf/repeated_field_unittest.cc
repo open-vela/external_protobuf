3e6730993a1cd897c07b8773d6c4bfb8bf5ba94d
liujinye <liujinye@xiaomi.com>
2021-05-14 21:56:03 +0800

    Merge tag 'refs/tags/sync-piper' into sync-stage
    # Conflicts:
    #	java/kotlin/src/test/kotlin/com/google/protobuf/Proto2Test.kt
    #	src/google/protobuf/repeated_field_unittest.cc

--- src/google/protobuf/repeated_field_unittest.cc
+++ src/google/protobuf/repeated_field_unittest.cc
@@ -889,8 +889,12 @@ TEST(RepeatedPtrField, UnambiguousConstructor) {
 
   // Construction from string iterators for the unique string overload "g"
   // works.
+<<<<<<< HEAD
+=======
   // std::string b[2] = {"abc", "xyz"};
+>>>>>>> refs/tags/sync-piper
   // Disabling this for now, this is actually ambiguous with libstdc++.
+  // std::string b[2] = {"abc", "xyz"};
   // EXPECT_TRUE(X::g({b, b + 2}));
 
   // Construction from string iterators for "f" is ambiguous, since both
