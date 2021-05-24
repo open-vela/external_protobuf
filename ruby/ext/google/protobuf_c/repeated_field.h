554b9161c7504f02feff46164d700e775907d765
openvela-robot <openvela-robot@xiaomi.com>
2021-05-24 13:45:21 +0800

    Merge pull request #8617 from deannagarcia/sync-stage
    Integrate from Piper for C++, Java, and Python

--- ruby/ext/google/protobuf_c/repeated_field.h
+++ ruby/ext/google/protobuf_c/repeated_field.h
@@ -44,8 +44,7 @@ VALUE RepeatedField_GetRubyWrapper(upb_array* msg, TypeInfo type_info,
 // Gets the underlying upb_array for this Ruby RepeatedField object, which must
 // have a type that matches |f|. If this is not a repeated field or the type
 // doesn't match, raises an exception.
-const upb_array* RepeatedField_GetUpbArray(VALUE value, const upb_fielddef* f,
-                                           upb_arena* arena);
+const upb_array* RepeatedField_GetUpbArray(VALUE value, const upb_fielddef* f);
 
 // Implements #inspect for this repeated field by appending its contents to |b|.
 void RepeatedField_Inspect(StringBuilder* b, const upb_array* array,
