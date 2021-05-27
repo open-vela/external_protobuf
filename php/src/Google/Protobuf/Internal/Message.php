6d9d2e593a025789ae78c455de4ced6a5c4b6fb0
liujinye <liujinye@xiaomi.com>
2021-05-27 15:36:36 +0800

    Merge pull request #8670 from haberman/php-wkt-submsg
    Fixed SEGV in sub-message getters for well-known types when message is unset.

--- php/src/Google/Protobuf/Internal/Message.php
+++ php/src/Google/Protobuf/Internal/Message.php
@@ -240,14 +240,10 @@ class Message
         $field = $this->desc->getFieldByNumber($number);
         $oneof = $this->desc->getOneofDecl()[$field->getOneofIndex()];
         $oneof_name = $oneof->getName();
-        if ($value === null) {
-            $this->$oneof_name = new OneofField($oneof);
-        } else {
-            $oneof_field = $this->$oneof_name;
-            $oneof_field->setValue($value);
-            $oneof_field->setFieldName($field->getName());
-            $oneof_field->setNumber($number);
-        }
+        $oneof_field = $this->$oneof_name;
+        $oneof_field->setValue($value);
+        $oneof_field->setFieldName($field->getName());
+        $oneof_field->setNumber($number);
     }
 
     protected function whichOneof($oneof_name)
