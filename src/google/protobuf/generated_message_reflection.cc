a770202c1dcb6f5dc9527972eb80d7d763d03d6c
openvela-robot <openvela-robot@xiaomi.com>
2021-09-09 08:21:42 +0800

    Sync from Piper @395706834
    PROTOBUF_SYNC_PIPER

--- src/google/protobuf/generated_message_reflection.cc
+++ src/google/protobuf/generated_message_reflection.cc
@@ -2491,12 +2491,18 @@ bool Reflection::HasBit(const Message& message,
     // (which uses HasField()) needs to be consistent with this.
     switch (field->cpp_type()) {
       case FieldDescriptor::CPPTYPE_STRING:
-        if (IsInlined(field)) {
-          return !GetField<InlinedStringField>(message, field)
-                      .GetNoArena()
-                      .empty();
+        switch (field->options().ctype()) {
+          default: {
+            if (IsInlined(field)) {
+              return !GetField<InlinedStringField>(message, field)
+                          .GetNoArena()
+                          .empty();
+            }
+
+            return GetField<ArenaStringPtr>(message, field).Get().size() > 0;
+          }
         }
-        return GetField<ArenaStringPtr>(message, field).Get().size() > 0;
+        return false;
       case FieldDescriptor::CPPTYPE_BOOL:
         return GetRaw<bool>(message, field) != false;
       case FieldDescriptor::CPPTYPE_INT32:
