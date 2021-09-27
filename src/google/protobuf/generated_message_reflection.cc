d2563a7ddc519b3c82d0a306c59b2c28a7ee7cb3
openvela-robot <openvela-robot@xiaomi.com>
2021-09-27 19:18:41 +0800

    Sync from Piper @399250367
    PROTOBUF_SYNC_PIPER

--- src/google/protobuf/generated_message_reflection.cc
+++ src/google/protobuf/generated_message_reflection.cc
@@ -995,6 +995,8 @@ void Reflection::SwapFieldsImpl(
   GOOGLE_DCHECK(!unsafe_shallow_swap || message1->GetArenaForAllocation() ==
                                      message2->GetArenaForAllocation());
 
+  const Message* prototype =
+      message_factory_->GetPrototype(message1->GetDescriptor());
   for (const auto* field : fields) {
     CheckInvalidAccess(schema_, field);
     if (field->is_extension()) {
@@ -1003,7 +1005,7 @@ void Reflection::SwapFieldsImpl(
             MutableExtensionSet(message2), field->number());
       } else {
         MutableExtensionSet(message1)->SwapExtension(
-            MutableExtensionSet(message2), field->number());
+            prototype, MutableExtensionSet(message2), field->number());
       }
     } else {
       if (schema_.InRealOneof(field)) {
@@ -1611,7 +1613,7 @@ std::string Reflection::GetString(const Message& message,
 
 const std::string& Reflection::GetStringReference(const Message& message,
                                                   const FieldDescriptor* field,
-                                                  std::string* /*scratch*/) const {
+                                                  std::string* scratch) const {
   USAGE_CHECK_ALL(GetStringReference, SINGULAR, STRING);
   if (field->is_extension()) {
     return GetExtensionSet(message).GetString(field->number(),
@@ -1699,7 +1701,7 @@ std::string Reflection::GetRepeatedString(const Message& message,
 
 const std::string& Reflection::GetRepeatedStringReference(
     const Message& message, const FieldDescriptor* field, int index,
-    std::string* /*scratch*/) const {
+    std::string* scratch) const {
   USAGE_CHECK_ALL(GetRepeatedStringReference, REPEATED, STRING);
   if (field->is_extension()) {
     return GetExtensionSet(message).GetRepeatedString(field->number(), index);
@@ -2237,7 +2239,7 @@ void Reflection::UnsafeArenaAddAllocatedMessage(Message* message,
 void* Reflection::MutableRawRepeatedField(Message* message,
                                           const FieldDescriptor* field,
                                           FieldDescriptor::CppType cpptype,
-                                          int /*ctype*/,
+                                          int ctype,
                                           const Descriptor* desc) const {
   USAGE_CHECK_REPEATED("MutableRawRepeatedField");
   CheckInvalidAccess(schema_, field);
@@ -2656,7 +2658,7 @@ HANDLE_TYPE(bool, FieldDescriptor::CPPTYPE_BOOL, -1);
 
 void* Reflection::MutableRawRepeatedString(Message* message,
                                            const FieldDescriptor* field,
-                                           bool /*is_string*/) const {
+                                           bool is_string) const {
   return MutableRawRepeatedField(message, field,
                                  FieldDescriptor::CPPTYPE_STRING,
                                  FieldOptions::STRING, nullptr);
@@ -3011,7 +3013,7 @@ void RegisterFileLevelMetadata(const DescriptorTable* table) {
 }
 
 void UnknownFieldSetSerializer(const uint8_t* base, uint32_t offset,
-                               uint32_t /*tag*/, uint32_t /*has_offset*/,
+                               uint32_t tag, uint32_t has_offset,
                                io::CodedOutputStream* output) {
   const void* ptr = base + offset;
   const InternalMetadata* metadata = static_cast<const InternalMetadata*>(ptr);
