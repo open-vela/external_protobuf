3ac17dba61f57bde96f5108acc5cfad8df51fa2e
liujinye <liujinye@xiaomi.com>
2021-10-26 13:01:52 +0800

    Merge pull request #9150 from Shopify/pz-message-encode-mem-leak
    [Ruby] Fix memory leak in MessageClass.encode

--- ruby/lib/google/protobuf/well_known_types.rb
+++ ruby/lib/google/protobuf/well_known_types.rb
@@ -82,14 +82,14 @@ module Google
         end
       end
 
-      def self.from_time(time)
-        new.from_time(time)
-      end
-
       def from_time(time)
         self.seconds = time.to_i
         self.nanos = time.nsec
         self
+      end
+
+      def self.from_time(time)
+        new.from_time(time)
       end
 
       def to_i
@@ -137,10 +137,6 @@ module Google
         end
       end
 
-      def self.from_ruby(value)
-        Value.new.from_ruby(value)
-      end
-
       def from_ruby(value)
         case value
         when NilClass
@@ -164,8 +160,6 @@ module Google
         else
           raise UnexpectedStructType
         end
-
-        self
       end
     end
 
@@ -236,5 +230,6 @@ module Google
         ret
       end
     end
+
   end
 end
