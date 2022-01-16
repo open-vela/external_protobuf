7215d56ac4183a6f7ad64ec0f0dd92b92845ef22
openvela-robot <openvela-robot@xiaomi.com>
2022-01-16 21:03:09 +0800

    Merge pull request #489 from haberman/rename3
    Renamed upb_FieldType_* -> kUpb_FieldType_*

--- python/protobuf.h
+++ python/protobuf.h
@@ -207,6 +207,5 @@ static inline PyObject* PyUpb_NewRef(PyObject* obj) {
 }
 
 const char* PyUpb_GetStrData(PyObject* obj);
-const char* PyUpb_CheckStrData(PyObject* obj);
 
 #endif  // PYUPB_PROTOBUF_H__
