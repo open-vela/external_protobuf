018f410c0b54d4d3360a28e2538ef20f937a7b9c
openvela-robot <openvela-robot@xiaomi.com>
2022-02-20 13:22:24 +0800

    MiniTable builder is successfully working on all files!

--- upbc/message_layout.h
+++ upbc/message_layout.h
@@ -31,6 +31,8 @@
 #include "absl/base/macros.h"
 #include "absl/container/flat_hash_map.h"
 #include "google/protobuf/descriptor.h"
+#include "upb/upb.hpp"
+#include "upb/mini_table.h"
 
 namespace upbc {
 
@@ -132,7 +134,7 @@ class MessageLayout {
   Size size_;
   int hasbit_count_;
   int hasbit_bytes_;
-  int required_count_;
+  int required_count_ = 0;
 };
 
 // Returns fields in order of "hotness", eg. how frequently they appear in
