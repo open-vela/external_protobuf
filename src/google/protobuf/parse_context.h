c8382d792ac1be698deb7e38c7f40d3cce4421a1
liujinye <liujinye@xiaomi.com>
2022-04-05 12:05:32 +0800

    Merge pull request #9727 from mlocati/build-packaged-php-extension
    Fix building packaged PHP extension
    
    (cherry picked from commit 7f9901c5f640fe0fbcd5dbdd303a269908fb3b62)

--- src/google/protobuf/parse_context.h
+++ src/google/protobuf/parse_context.h
@@ -34,7 +34,6 @@
 #include <cstdint>
 #include <cstring>
 #include <string>
-#include <type_traits>
 
 #include <google/protobuf/io/coded_stream.h>
 #include <google/protobuf/io/zero_copy_stream.h>
@@ -375,7 +374,6 @@ class PROTOBUF_EXPORT EpsCopyInputStream {
 
 using LazyEagerVerifyFnType = const char* (*)(const char* ptr,
                                               ParseContext* ctx);
-using LazyEagerVerifyFnRef = std::remove_pointer<LazyEagerVerifyFnType>::type&;
 
 // ParseContext holds all data that is global to the entire parse. Most
 // importantly it contains the input stream, but also recursion depth and also
