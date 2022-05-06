081a51b955c2bdcfadf0fc089d1bd651d1a84bc5
liujinye <liujinye@xiaomi.com>
2022-05-06 10:21:35 +0800

    Merge pull request #9536 from bshaffer/patch-2
    chore: [PHP] fix phpdoc for MapField keys

--- src/google/protobuf/map.h
+++ src/google/protobuf/map.h
@@ -1363,8 +1363,11 @@ class Map {
   }
   template <class InputIt>
   void insert(InputIt first, InputIt last) {
-    for (; first != last; ++first) {
-      try_emplace(first->first, first->second);
+    for (InputIt it = first; it != last; ++it) {
+      iterator exist_it = find(it->first);
+      if (exist_it == end()) {
+        operator[](it->first) = it->second;
+      }
     }
   }
   void insert(std::initializer_list<value_type> values) {
