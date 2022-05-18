a61ca5600735a305d2afbc2d7bffa0282d3b5fc6
openvela-robot <openvela-robot@xiaomi.com>
2022-05-18 21:39:30 +0800

    Fixed PHP SEGV by not writing to shared memory for zend_class_entry.

--- kokoro/linux/bazel/build.sh
+++ kokoro/linux/bazel/build.sh
@@ -7,6 +7,16 @@ set -ex
 use_bazel.sh 4.0.0
 bazel version
 
+# Print bazel testlogs to stdout when tests failed.
+function print_test_logs {
+  # TODO(yannic): Only print logs of failing tests.
+  testlogs_dir=$(bazel info bazel-testlogs)
+  testlogs=$(find "${testlogs_dir}" -name "*.log")
+  for log in $testlogs; do
+    cat "${log}"
+  done
+}
+
 # Change to repo root
 cd $(dirname $0)/../../..
 
@@ -15,13 +25,19 @@ git submodule update --init --recursive
 #  Disabled for now, re-enable if appropriate.
 #  //:build_files_updated_unittest \
 
-bazel test \
-  -k --copt=-Werror --host_copt=-Werror --test_output=errors \
+trap print_test_logs EXIT
+bazel test -k --copt=-Werror --host_copt=-Werror \
   //build_defs:all \
   //java:tests \
-  //src/... \
+  //:protoc \
+  //:protobuf \
   //:protobuf_python \
-  @com_google_protobuf_examples//...
+  //:protobuf_test
+trap - EXIT
+
+pushd examples
+bazel build //...
+popd
 
 # Verify that we can build successfully from generated tar files.
 ./autogen.sh && ./configure && make -j$(nproc) dist
