ddb52b2611f9bd33c352efb68984c5f8ffc0f28a
openvela-robot <openvela-robot@xiaomi.com>
2022-05-20 15:54:17 +0800

    Merge pull request #10018 from protocolbuffers/main-202205201724
    Merge 21.x into Main

--- kokoro/linux/bazel/build.sh
+++ kokoro/linux/bazel/build.sh
@@ -7,40 +7,29 @@ set -ex
 use_bazel.sh 4.0.0
 bazel version
 
-# Print bazel testlogs to stdout when tests failed.
-function print_test_logs {
-  # TODO(yannic): Only print logs of failing tests.
-  testlogs_dir=$(bazel info bazel-testlogs)
-  testlogs=$(find "${testlogs_dir}" -name "*.log")
-  for log in $testlogs; do
-    cat "${log}"
-  done
-}
-
 # Change to repo root
 cd $(dirname $0)/../../..
+source kokoro/common/pyenv.sh
 
 git submodule update --init --recursive
 
 #  Disabled for now, re-enable if appropriate.
 #  //:build_files_updated_unittest \
 
-trap print_test_logs EXIT
-bazel test -k --copt=-Werror --host_copt=-Werror \
+bazel test \
+  -k --copt=-Werror --host_copt=-Werror --test_output=errors \
   //build_defs:all \
   //java:tests \
-  //:protoc \
-  //:protobuf \
+  //src/... \
   //:protobuf_python \
-  //:protobuf_test
-trap - EXIT
-
-pushd examples
-bazel build //...
-popd
+  @com_google_protobuf_examples//...
 
 # Verify that we can build successfully from generated tar files.
-./autogen.sh && ./configure && make -j$(nproc) dist
+(
+  pyenv versions
+  pyenv shell 2.7.9  # python2 required for old googletest autotools support
+  ./autogen.sh && ./configure && make -j$(nproc) dist
+)
 DIST=`ls *.tar.gz`
 tar -xf $DIST
 cd ${DIST//.tar.gz}
