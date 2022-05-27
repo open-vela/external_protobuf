9a1137871928bfdd4c40c6a96f443b32da24984d
openvela-robot <openvela-robot@xiaomi.com>
2022-05-27 23:24:44 +0800

    Merge branch '21.x' into 21.x-202205272218

--- kokoro/linux/aarch64/protoc_crosscompile_aarch64.sh
+++ kokoro/linux/aarch64/protoc_crosscompile_aarch64.sh
@@ -7,6 +7,3 @@ set -ex
 ./autogen.sh
 CXXFLAGS="-fPIC -g -O2" ./configure --host=aarch64
 make -j8
-
-# The Java build setup expects the protoc binary to be in the src/ directory.
-ln -s $PWD/protoc ./src/protoc
