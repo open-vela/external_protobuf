e1c0b33a04dd935a3e7102058500aa434a1be46f
liujinye <liujinye@xiaomi.com>
2022-07-11 18:34:32 +0800

    Merge pull request #10233 from esorot/sync-stage
    Integrate from Piper for C++, Java, and Python

--- kokoro/linux/bazel_distcheck/build.sh
+++ kokoro/linux/bazel_distcheck/build.sh
@@ -55,7 +55,6 @@ bazel_args=(
   --
   //...
   -//objectivec/...  # only works on macOS
-  -//csharp/...      # release builds require external dependencies
   @com_google_protobuf_examples//...
 )
 ${SCRIPT_ROOT}/kokoro/common/bazel_wrapper.sh "${bazel_args[@]}"
