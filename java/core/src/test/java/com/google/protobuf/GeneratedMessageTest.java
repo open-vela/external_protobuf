b4703016ba5a46e22549e1078ab136943c2a4851
liujinye <liujinye@xiaomi.com>
2022-07-11 22:04:40 +0800

    Adding Github Action to keep CMake generated file lists up to date (#10243)

--- java/core/src/test/java/com/google/protobuf/GeneratedMessageTest.java
+++ java/core/src/test/java/com/google/protobuf/GeneratedMessageTest.java
@@ -1529,7 +1529,7 @@ public class GeneratedMessageTest {
       assertThat(builder.getFooInt()).isEqualTo(123);
       TestOneof2 message = builder.buildPartial();
       assertThat(message.hasFooInt()).isTrue();
-      assertThat(123).isEqualTo(message.getFooInt());
+      assertThat(message.getFooInt()).isEqualTo(123);
 
       assertThat(builder.clearFooInt().hasFooInt()).isFalse();
       TestOneof2 message2 = builder.build();
@@ -1943,7 +1943,7 @@ public class GeneratedMessageTest {
 
   @Test
   public void
-  extendableBuilder_extensionFieldContainingBuilder_setRepeatedFieldOverwritesElement() {
+      extendableBuilder_extensionFieldContainingBuilder_setRepeatedFieldOverwritesElement() {
     TestAllExtensions.Builder builder = TestAllExtensions.newBuilder();
     builder.addRepeatedField(REPEATED_NESTED_MESSAGE_EXTENSION, NestedMessage.getDefaultInstance());
     // Calling getRepeatedFieldBuilder and ignoring the returned Builder should have no
--- php/ext/google/protobuf/def.c
+++ php/ext/google/protobuf/def.c
@@ -162,7 +162,7 @@ static void EnumDescriptor_FromEnumDef(zval *val, const upb_EnumDef *m) {
     ZVAL_NULL(val);
   } else {
     char *classname =
-        GetPhpClassname(upb_EnumDef_File(m), upb_EnumDef_FullName(m), false);
+        GetPhpClassname(upb_EnumDef_File(m), upb_EnumDef_FullName(m));
     zend_string *str = zend_string_init(classname, strlen(classname), 0);
     zend_class_entry *ce = zend_lookup_class(str);  // May autoload the class.
 
@@ -500,24 +500,14 @@ static void Descriptor_destructor(zend_object* obj) {
 
 static zend_class_entry *Descriptor_GetGeneratedClass(const upb_MessageDef *m) {
   char *classname =
-      GetPhpClassname(upb_MessageDef_File(m), upb_MessageDef_FullName(m), false);
+      GetPhpClassname(upb_MessageDef_File(m), upb_MessageDef_FullName(m));
   zend_string *str = zend_string_init(classname, strlen(classname), 0);
   zend_class_entry *ce = zend_lookup_class(str);  // May autoload the class.
 
   zend_string_release (str);
 
   if (!ce) {
-    char *classname2 =
-      GetPhpClassname(upb_MessageDef_File(m), upb_MessageDef_FullName(m), true);
-    zend_string *str2 = zend_string_init(classname2, strlen(classname2), 0);
-    ce = zend_lookup_class(str2);  // May autoload the class.
-
-    zend_string_release (str2);
-    free(classname2);
-
-    if (!ce) {
-      zend_error(E_ERROR, "Couldn't load generated class %s", classname);
-    }
+    zend_error(E_ERROR, "Couldn't load generated class %s", classname);
   }
 
   free(classname);
