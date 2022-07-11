7cbe1e78f1332cc7d19627f1782d9cd42109b8ad
openvela-robot <openvela-robot@xiaomi.com>
2022-07-11 18:32:33 +0800

    PR review: use pointer to beginning of std::string instead of calling const_cast

--- java/kotlin/src/test/kotlin/com/google/protobuf/Proto3Test.kt
+++ java/kotlin/src/test/kotlin/com/google/protobuf/Proto3Test.kt
@@ -31,13 +31,13 @@
 package com.google.protobuf.kotlin
 
 import com.google.common.truth.Truth.assertThat
-import com.google.protobuf.kotlin.generator.`in`.EvilNamesProto3OuterClass.Class
-import com.google.protobuf.kotlin.generator.`in`.EvilNamesProto3OuterClass.EvilNamesProto3
-import com.google.protobuf.kotlin.generator.`in`.EvilNamesProto3OuterClass.HardKeywordsAllTypesProto3
-import com.google.protobuf.kotlin.generator.`in`.HardKeywordsAllTypesProto3Kt
-import com.google.protobuf.kotlin.generator.`in`.class_
-import com.google.protobuf.kotlin.generator.`in`.evilNamesProto3
-import com.google.protobuf.kotlin.generator.`in`.hardKeywordsAllTypesProto3
+import com.google.protobuf.kotlin.generator.EvilNamesProto3OuterClass.Class
+import com.google.protobuf.kotlin.generator.EvilNamesProto3OuterClass.EvilNamesProto3
+import com.google.protobuf.kotlin.generator.EvilNamesProto3OuterClass.HardKeywordsAllTypesProto3
+import com.google.protobuf.kotlin.generator.HardKeywordsAllTypesProto3Kt
+import com.google.protobuf.kotlin.generator.class_
+import com.google.protobuf.kotlin.generator.evilNamesProto3
+import com.google.protobuf.kotlin.generator.hardKeywordsAllTypesProto3
 import proto3_unittest.TestAllTypesKt
 import proto3_unittest.TestAllTypesKt.nestedMessage
 import proto3_unittest.UnittestProto3.TestAllTypes
@@ -54,7 +54,6 @@ import org.junit.runners.JUnit4
 
 @RunWith(JUnit4::class)
 class Proto3Test {
-  @Suppress("CheckResult")
   @Test
   fun testGettersAndSetters() {
     testAllTypes {
@@ -71,7 +70,6 @@ class Proto3Test {
     }
   }
 
-  @Suppress("CheckResult")
   @Test
   fun testRepeatedGettersAndSetters() {
     testAllTypes {
@@ -261,7 +259,6 @@ class Proto3Test {
     assertThat(class_ {}).isEqualTo(Class.newBuilder().build())
   }
 
-  @Suppress("CheckResult")
   @Test
   fun testHardKeywordGettersAndSetters() {
     hardKeywordsAllTypesProto3 {
@@ -294,7 +291,6 @@ class Proto3Test {
     }
   }
 
-  @Suppress("CheckResult")
   @Test
   fun testHardKeywordHazzers() {
     hardKeywordsAllTypesProto3 {
@@ -312,7 +308,6 @@ class Proto3Test {
     }
   }
 
-  @Suppress("CheckResult")
   @Test
   fun testHardKeywordClears() {
     hardKeywordsAllTypesProto3 {
