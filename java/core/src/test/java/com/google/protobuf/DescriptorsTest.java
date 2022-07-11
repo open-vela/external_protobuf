1d0620d07002aa6babd491c89494354d9ee58d9d
openvela-robot <openvela-robot@xiaomi.com>
2022-07-11 17:31:52 +0800

    Merge branch '21.x' into 21.x-php-add-previous-classname-to-descriptor-pool

--- java/core/src/test/java/com/google/protobuf/DescriptorsTest.java
+++ java/core/src/test/java/com/google/protobuf/DescriptorsTest.java
@@ -463,15 +463,15 @@ public class DescriptorsTest {
   /** Tests that parsing an unknown enum throws an exception */
   @Test
   public void testParseUnknownEnum() {
-    FieldDescriptorProto.Builder field =
-        FieldDescriptorProto.newBuilder()
-            .setLabel(FieldDescriptorProto.Label.LABEL_OPTIONAL)
-            .setTypeName("UnknownEnum")
-            .setType(FieldDescriptorProto.Type.TYPE_ENUM)
-            .setName("bar")
-            .setNumber(1);
-    DescriptorProto.Builder messageType =
-        DescriptorProto.newBuilder().setName("Foo").addField(field);
+    FieldDescriptorProto.Builder field = FieldDescriptorProto.newBuilder()
+        .setLabel(FieldDescriptorProto.Label.LABEL_OPTIONAL)
+        .setTypeName("UnknownEnum")
+        .setType(FieldDescriptorProto.Type.TYPE_ENUM)
+        .setName("bar")
+        .setNumber(1);
+    DescriptorProto.Builder messageType = DescriptorProto.newBuilder()
+        .setName("Foo")
+        .addField(field);
     FileDescriptorProto fooProto =
         FileDescriptorProto.newBuilder()
             .setName("foo.proto")
@@ -485,6 +485,7 @@ public class DescriptorsTest {
       assertThat(expected.getMessage()).contains("\"UnknownEnum\" is not an enum type.");
     }
   }
+
 
   /**
    * Tests the translate/crosslink for an example where a message field's name and type name are the
@@ -559,10 +560,8 @@ public class DescriptorsTest {
         Descriptors.FileDescriptor.buildFrom(barProto, new FileDescriptor[] {fooFile});
 
     // Items in the FileDescriptor array can be in any order.
-    FileDescriptor unused1 =
-        Descriptors.FileDescriptor.buildFrom(bazProto, new FileDescriptor[] {fooFile, barFile});
-    FileDescriptor unused2 =
-        Descriptors.FileDescriptor.buildFrom(bazProto, new FileDescriptor[] {barFile, fooFile});
+    Descriptors.FileDescriptor.buildFrom(bazProto, new FileDescriptor[] {fooFile, barFile});
+    Descriptors.FileDescriptor.buildFrom(bazProto, new FileDescriptor[] {barFile, fooFile});
   }
 
   @Test
@@ -624,8 +623,7 @@ public class DescriptorsTest {
                             .setName("bar")
                             .setNumber(1)))
             .build();
-    FileDescriptor unused =
-        Descriptors.FileDescriptor.buildFrom(fooProto, new FileDescriptor[0], true);
+    Descriptors.FileDescriptor.buildFrom(fooProto, new FileDescriptor[0], true);
   }
 
   @Test
@@ -659,8 +657,7 @@ public class DescriptorsTest {
         Descriptors.FileDescriptor.buildFrom(forwardProto, new FileDescriptor[] {barFile});
 
     try {
-      FileDescriptor unused =
-          Descriptors.FileDescriptor.buildFrom(fooProto, new FileDescriptor[] {forwardFile});
+      Descriptors.FileDescriptor.buildFrom(fooProto, new FileDescriptor[] {forwardFile});
       assertWithMessage("DescriptorValidationException expected").fail();
     } catch (DescriptorValidationException e) {
       assertThat(e).hasMessageThat().contains("Bar");
@@ -698,8 +695,7 @@ public class DescriptorsTest {
     FileDescriptor barFile = Descriptors.FileDescriptor.buildFrom(barProto, new FileDescriptor[0]);
     FileDescriptor forwardFile =
         Descriptors.FileDescriptor.buildFrom(forwardProto, new FileDescriptor[] {barFile});
-    FileDescriptor unused =
-        Descriptors.FileDescriptor.buildFrom(fooProto, new FileDescriptor[] {forwardFile});
+    Descriptors.FileDescriptor.buildFrom(fooProto, new FileDescriptor[] {forwardFile});
   }
 
   /** Tests the translate/crosslink for an example with a more complex namespace referencing. */
@@ -830,8 +826,7 @@ public class DescriptorsTest {
                             .build())
                     .build())
             .build();
-    FileDescriptor unused =
-        Descriptors.FileDescriptor.buildFrom(fileDescriptorProto, new FileDescriptor[0]);
+    Descriptors.FileDescriptor.buildFrom(fileDescriptorProto, new FileDescriptor[0]);
   }
 
   @Test
