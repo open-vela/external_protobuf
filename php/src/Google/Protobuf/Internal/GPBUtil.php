52d4c68dffbdc10411272676037b753fc725735d
liujinye <liujinye@xiaomi.com>
2022-07-19 11:40:04 +0800

    Merge pull request #10272 from jskeet/fix-dist
    Fix distribution test failures

--- php/src/Google/Protobuf/Internal/GPBUtil.php
+++ php/src/Google/Protobuf/Internal/GPBUtil.php
@@ -285,12 +285,11 @@ class GPBUtil
             "include"=>0, "include_once"=>0, "instanceof"=>0, "insteadof"=>0,
             "interface"=>0, "isset"=>0, "list"=>0, "match"=>0, "namespace"=>0,
             "new"=>0, "or"=>0, "parent"=>0, "print"=>0, "private"=>0,
-            "protected"=>0,"public"=>0, "readonly" => 0,"require"=>0,
-            "require_once"=>0,"return"=>0, "self"=>0, "static"=>0, "switch"=>0,
-            "throw"=>0,"trait"=>0, "try"=>0,"unset"=>0, "use"=>0, "var"=>0,
-            "while"=>0,"xor"=>0, "yield"=>0, "int"=>0, "float"=>0, "bool"=>0,
-            "string"=>0,"true"=>0, "false"=>0, "null"=>0, "void"=>0,
-            "iterable"=>0
+            "protected"=>0,"public"=>0, "require"=>0, "require_once"=>0,
+            "return"=>0, "self"=>0, "static"=>0, "switch"=>0, "throw"=>0,
+            "trait"=>0, "try"=>0,"unset"=>0, "use"=>0, "var"=>0, "while"=>0,
+            "xor"=>0, "yield"=>0, "int"=>0, "float"=>0, "bool"=>0, "string"=>0,
+            "true"=>0, "false"=>0, "null"=>0, "void"=>0, "iterable"=>0
         );
 
         if (array_key_exists(strtolower($classname), $reserved_words)) {
@@ -302,27 +301,6 @@ class GPBUtil
         }
 
         return "";
-    }
-
-    private static function getPreviouslyUnreservedClassNamePrefix(
-        $classname,
-        $file_proto)
-    {
-        $previously_unreserved_words = array(
-            "readonly"=>0
-        );
-
-        if (array_key_exists(strtolower($classname), $previously_unreserved_words)) {
-            $option = $file_proto->getOptions();
-            $prefix = is_null($option) ? "" : $option->getPhpClassPrefix();
-            if ($prefix !== "") {
-                return $prefix;
-            }
-
-            return "";
-        }
-
-        return self::getClassNamePrefix($classname, $file_proto);
     }
 
     public static function getLegacyClassNameWithoutPackage(
@@ -344,17 +322,6 @@ class GPBUtil
         return implode('\\', $parts);
     }
 
-    private static function getPreviouslyUnreservedClassNameWithoutPackage(
-        $name,
-        $file_proto)
-    {
-        $parts = explode('.', $name);
-        foreach ($parts as $i => $part) {
-            $parts[$i] = static::getPreviouslyUnreservedClassNamePrefix($parts[$i], $file_proto) . $parts[$i];
-        }
-        return implode('\\', $parts);
-    }
-
     public static function getFullClassName(
         $proto,
         $containing,
@@ -362,8 +329,7 @@ class GPBUtil
         &$message_name_without_package,
         &$classname,
         &$legacy_classname,
-        &$fullname,
-        &$previous_classname)
+        &$fullname)
     {
         // Full name needs to start with '.'.
         $message_name_without_package = $proto->getName();
@@ -384,9 +350,6 @@ class GPBUtil
         $legacy_class_name_without_package =
             static::getLegacyClassNameWithoutPackage(
                 $message_name_without_package, $file_proto);
-        $previous_class_name_without_package =
-            static::getPreviouslyUnreservedClassNameWithoutPackage(
-                $message_name_without_package, $file_proto);
 
         $option = $file_proto->getOptions();
         if (!is_null($option) && $option->hasPhpNamespace()) {
@@ -395,13 +358,10 @@ class GPBUtil
                 $classname = $namespace . "\\" . $class_name_without_package;
                 $legacy_classname =
                     $namespace . "\\" . $legacy_class_name_without_package;
-                $previous_classname =
-                    $namespace . "\\" . $previous_class_name_without_package;
                 return;
             } else {
                 $classname = $class_name_without_package;
                 $legacy_classname = $legacy_class_name_without_package;
-                $previous_classname = $previous_class_name_without_package;
                 return;
             }
         }
@@ -409,7 +369,6 @@ class GPBUtil
         if ($package === "") {
             $classname = $class_name_without_package;
             $legacy_classname = $legacy_class_name_without_package;
-            $previous_classname = $previous_class_name_without_package;
         } else {
             $parts = array_map('ucwords', explode('.', $package));
             foreach ($parts as $i => $part) {
@@ -422,11 +381,6 @@ class GPBUtil
             $legacy_classname =
                 implode('\\', array_map('ucwords', explode('.', $package))).
                 "\\".$legacy_class_name_without_package;
-            $previous_classname =
-                implode('\\', array_map('ucwords', explode('.', $package))).
-                "\\".self::getPreviouslyUnreservedClassNamePrefix(
-                    $previous_class_name_without_package, $file_proto).
-                    $previous_class_name_without_package;
         }
     }
 
