d92ecbd0af26cbfc1947721a7b2f6b0cb02eb749
liujinye <liujinye@xiaomi.com>
2022-07-20 17:28:35 +0800

    Update java/protoc

--- php/ext/google/protobuf/names.h
+++ php/ext/google/protobuf/names.h
@@ -35,7 +35,6 @@
 
 // Translates a protobuf symbol name (eg. foo.bar.Baz) into a PHP class name
 // (eg. \Foo\Bar\Baz).
-char *GetPhpClassname(const upb_FileDef *file, const char *fullname, bool previous);
-bool IsPreviouslyUnreservedClassName(const char* fullname);
+char *GetPhpClassname(const upb_FileDef *file, const char *fullname);
 
 #endif  // PHP_PROTOBUF_NAMES_H_
