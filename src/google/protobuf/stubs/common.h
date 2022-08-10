6df0b54c51c34ff4816aa556c9b9fec58b4b3205
openvela-robot <openvela-robot@xiaomi.com>
2022-08-10 21:42:03 +0800

    Merge pull request #10375 from oclea/main
    Add extension for Oclea Service Layer RPCs

--- src/google/protobuf/stubs/common.h
+++ src/google/protobuf/stubs/common.h
@@ -106,12 +106,7 @@ void PROTOBUF_EXPORT VerifyVersion(int headerVersion, int minLibraryVersion,
                                    const char* filename);
 
 // Converts a numeric version number to a string.
-// If cppMajor is true the string will have the C++ major version, otherwise
-// the string will be the protoc version.
-std::string PROTOBUF_EXPORT VersionString(int version, bool cppMajor = true);
-
-// Prints the protoc compiler version (no major version)
-std::string PROTOBUF_EXPORT ProtocVersionString(int version);
+std::string PROTOBUF_EXPORT VersionString(int version);
 
 }  // namespace internal
 
