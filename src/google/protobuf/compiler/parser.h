4b8bd0ac830f2daa19a54b9b33205efb78a3f74e
liujinye <liujinye@xiaomi.com>
2022-09-15 15:27:38 +0800

    clarify comment

--- src/google/protobuf/compiler/parser.h
+++ src/google/protobuf/compiler/parser.h
@@ -180,9 +180,6 @@ class PROTOBUF_EXPORT Parser {
   // is greater than max_value, an error will be reported.
   bool ConsumeInteger64(uint64_t max_value, uint64_t* output,
                         const char* error);
-  // Try to consume a 64-bit integer and store its value in "output".  No
-  // error is reported on failure, allowing caller to consume token another way.
-  bool TryConsumeInteger64(uint64_t max_value, uint64_t* output);
   // Consume a number and store its value in "output".  This will accept
   // tokens of either INTEGER or FLOAT type.
   bool ConsumeNumber(double* output, const char* error);
@@ -397,6 +394,7 @@ class PROTOBUF_EXPORT Parser {
                      const LocationRecorder& message_location);
   bool ParseReservedNames(DescriptorProto* message,
                           const LocationRecorder& parent_location);
+  bool ParseReservedName(std::string* name, const char* error_message);
   bool ParseReservedNumbers(DescriptorProto* message,
                             const LocationRecorder& parent_location);
   bool ParseReserved(EnumDescriptorProto* message,
