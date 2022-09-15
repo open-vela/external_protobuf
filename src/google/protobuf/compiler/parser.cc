47c7f8889ce1150bdeb1d47cee303392ae9cb758
openvela-robot <openvela-robot@xiaomi.com>
2022-09-15 16:10:59 +0800

    Merge pull request #10555 from jhump/jh/fix-consistency-with-very-large-decimal-numbers
    protoc: fix consistency with parsing very large decimal numbers

--- src/google/protobuf/compiler/parser.cc
+++ src/google/protobuf/compiler/parser.cc
@@ -51,7 +51,6 @@
 #include "absl/strings/ascii.h"
 #include "absl/strings/escaping.h"
 #include "absl/strings/str_cat.h"
-#include "absl/strings/str_format.h"
 #include "google/protobuf/descriptor.h"
 #include "google/protobuf/descriptor.pb.h"
 #include "google/protobuf/io/tokenizer.h"
@@ -289,6 +288,16 @@ bool Parser::ConsumeInteger64(uint64_t max_value, uint64_t* output,
   }
 }
 
+bool Parser::TryConsumeInteger64(uint64_t max_value, uint64_t* output) {
+  if (LookingAtType(io::Tokenizer::TYPE_INTEGER) &&
+      io::Tokenizer::ParseInteger(input_->current().text, max_value,
+                                  output)) {
+    input_->Next();
+    return true;
+  }
+  return false;
+}
+
 bool Parser::ConsumeNumber(double* output, const char* error) {
   if (LookingAtType(io::Tokenizer::TYPE_FLOAT)) {
     *output = io::Tokenizer::ParseFloat(input_->current().text);
@@ -297,13 +306,19 @@ bool Parser::ConsumeNumber(double* output, const char* error) {
   } else if (LookingAtType(io::Tokenizer::TYPE_INTEGER)) {
     // Also accept integers.
     uint64_t value = 0;
-    if (!io::Tokenizer::ParseInteger(input_->current().text,
+    if (io::Tokenizer::ParseInteger(input_->current().text,
                                      std::numeric_limits<uint64_t>::max(),
                                      &value)) {
+      *output = value;
+    } else if (input_->current().text[0] == '0') {
+      // octal or hexadecimal; don't bother parsing as float
+      AddError("Integer out of range.");
+      // We still return true because we did, in fact, parse a number.
+    } else if (!io::Tokenizer::TryParseFloat(input_->current().text, output)) {
+      // out of int range, and not valid float? 🤷
       AddError("Integer out of range.");
       // We still return true because we did, in fact, parse a number.
     }
-    *output = value;
     input_->Next();
     return true;
   } else if (LookingAt("inf")) {
@@ -409,6 +424,11 @@ Parser::LocationRecorder::LocationRecorder(Parser* parser)
 
 Parser::LocationRecorder::LocationRecorder(const LocationRecorder& parent) {
   Init(parent, parent.source_code_info_);
+}
+
+Parser::LocationRecorder::LocationRecorder(const LocationRecorder& parent,
+                                           SourceCodeInfo* source_code_info) {
+  Init(parent, source_code_info);
 }
 
 Parser::LocationRecorder::LocationRecorder(const LocationRecorder& parent,
@@ -1240,13 +1260,22 @@ bool Parser::ParseDefaultAssignment(
     field->clear_default_value();
   }
 
+  LocationRecorder location(field_location,
+                            FieldDescriptorProto::kDefaultValueFieldNumber);
+
   DO(Consume("default"));
   DO(Consume("="));
 
-  LocationRecorder location(field_location,
-                            FieldDescriptorProto::kDefaultValueFieldNumber);
-  location.RecordLegacyLocation(field,
-                                DescriptorPool::ErrorCollector::DEFAULT_VALUE);
+  // We don't need to create separate spans in source code info for name and value,
+  // since there's no way to represent them distinctly in a location path. But we will
+  // want a separate recorder for the value, just to have more precise location info
+  // in error messages. So we let it create a location in no_op, so it doesn't add a
+  // span to the file descriptor.
+  SourceCodeInfo no_op;
+  LocationRecorder value_location(location, &no_op);
+  value_location.RecordLegacyLocation(
+      field, DescriptorPool::ErrorCollector::DEFAULT_VALUE);
+
   std::string* default_value = field->mutable_default_value();
 
   if (!field->has_type()) {
@@ -1380,13 +1409,23 @@ bool Parser::ParseJsonName(FieldDescriptorProto* field,
 
   LocationRecorder location(field_location,
                             FieldDescriptorProto::kJsonNameFieldNumber);
-  location.RecordLegacyLocation(field,
-                                DescriptorPool::ErrorCollector::OPTION_NAME);
 
-  DO(Consume("json_name"));
+  // We don't need to create separate spans in source code info for name and value,
+  // since there's no way to represent them distinctly in a location path. But we will
+  // want a separate recorder for them, just to have more precise location info
+  // in error messages. So we let them create a location in no_op, so they don't
+  // add a span to the file descriptor.
+  SourceCodeInfo no_op;
+  {
+    LocationRecorder name_location(location, &no_op);
+    name_location.RecordLegacyLocation(
+        field, DescriptorPool::ErrorCollector::OPTION_NAME);
+
+    DO(Consume("json_name"));
+  }
   DO(Consume("="));
 
-  LocationRecorder value_location(location);
+  LocationRecorder value_location(location, &no_op);
   value_location.RecordLegacyLocation(
       field, DescriptorPool::ErrorCollector::OPTION_VALUE);
 
@@ -1552,18 +1591,20 @@ bool Parser::ParseOption(Message* options,
             is_negative
                 ? static_cast<uint64_t>(std::numeric_limits<int64_t>::max()) + 1
                 : std::numeric_limits<uint64_t>::max();
-        DO(ConsumeInteger64(max_value, &value, "Expected integer."));
-        if (is_negative) {
-          value_location.AddPath(
-              UninterpretedOption::kNegativeIntValueFieldNumber);
-          uninterpreted_option->set_negative_int_value(
-              static_cast<int64_t>(0 - value));
-        } else {
-          value_location.AddPath(
-              UninterpretedOption::kPositiveIntValueFieldNumber);
-          uninterpreted_option->set_positive_int_value(value);
+        if (TryConsumeInteger64(max_value, &value)) {
+          if (is_negative) {
+            value_location.AddPath(
+                UninterpretedOption::kNegativeIntValueFieldNumber);
+            uninterpreted_option->set_negative_int_value(
+                static_cast<int64_t>(0 - value));
+          } else {
+            value_location.AddPath(
+                UninterpretedOption::kPositiveIntValueFieldNumber);
+            uninterpreted_option->set_positive_int_value(value);
+          }
+          break;
         }
-        break;
+        // value too large for an integer; fall through below to treat as floating point
       }
 
       case io::Tokenizer::TYPE_FLOAT: {
@@ -1729,24 +1770,11 @@ bool Parser::ParseReserved(DescriptorProto* message,
   }
 }
 
-bool Parser::ParseReservedName(std::string* name, const char* error_message) {
-  // Capture the position of the token, in case we have to report an
-  // error after it is consumed.
-  int line = input_->current().line;
-  int col = input_->current().column;
-  DO(ConsumeString(name, error_message));
-  if (!io::Tokenizer::IsIdentifier(*name)) {
-    AddError(line, col, absl::StrFormat("Reserved name \"%s\" is not a valid identifier.", *name));
-    return false;
-  }
-  return true;
-}
-
 bool Parser::ParseReservedNames(DescriptorProto* message,
                                 const LocationRecorder& parent_location) {
   do {
     LocationRecorder location(parent_location, message->reserved_name_size());
-    DO(ParseReservedName(message->add_reserved_name(), "Expected field name."));
+    DO(ConsumeString(message->add_reserved_name(), "Expected field name."));
   } while (TryConsume(","));
   DO(ConsumeEndOfDeclaration(";", &parent_location));
   return true;
@@ -1801,42 +1829,42 @@ bool Parser::ParseReservedNumbers(DescriptorProto* message,
   return true;
 }
 
-bool Parser::ParseReserved(EnumDescriptorProto* proto,
-                           const LocationRecorder& enum_location) {
+bool Parser::ParseReserved(EnumDescriptorProto* message,
+                           const LocationRecorder& message_location) {
   io::Tokenizer::Token start_token = input_->current();
   // Parse the declaration.
   DO(Consume("reserved"));
   if (LookingAtType(io::Tokenizer::TYPE_STRING)) {
-    LocationRecorder location(enum_location,
+    LocationRecorder location(message_location,
                               EnumDescriptorProto::kReservedNameFieldNumber);
     location.StartAt(start_token);
-    return ParseReservedNames(proto, location);
+    return ParseReservedNames(message, location);
   } else {
-    LocationRecorder location(enum_location,
+    LocationRecorder location(message_location,
                               EnumDescriptorProto::kReservedRangeFieldNumber);
     location.StartAt(start_token);
-    return ParseReservedNumbers(proto, location);
+    return ParseReservedNumbers(message, location);
   }
 }
 
-bool Parser::ParseReservedNames(EnumDescriptorProto* proto,
+bool Parser::ParseReservedNames(EnumDescriptorProto* message,
                                 const LocationRecorder& parent_location) {
   do {
-    LocationRecorder location(parent_location, proto->reserved_name_size());
-    DO(ParseReservedName(proto->add_reserved_name(), "Expected enum value."));
+    LocationRecorder location(parent_location, message->reserved_name_size());
+    DO(ConsumeString(message->add_reserved_name(), "Expected enum value."));
   } while (TryConsume(","));
   DO(ConsumeEndOfDeclaration(";", &parent_location));
   return true;
 }
 
-bool Parser::ParseReservedNumbers(EnumDescriptorProto* proto,
+bool Parser::ParseReservedNumbers(EnumDescriptorProto* message,
                                   const LocationRecorder& parent_location) {
   bool first = true;
   do {
-    LocationRecorder location(parent_location, proto->reserved_range_size());
+    LocationRecorder location(parent_location, message->reserved_range_size());
 
     EnumDescriptorProto::EnumReservedRange* range =
-        proto->add_reserved_range();
+        message->add_reserved_range();
     int start, end;
     io::Tokenizer::Token start_token;
     {
