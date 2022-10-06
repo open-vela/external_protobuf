a6e98a4d7f4a4837898890a6a151ac250ee87582
liujinye <liujinye@xiaomi.com>
2022-10-06 22:08:39 +0800

    Sync from Piper @479479714
    PROTOBUF_SYNC_PIPER

--- src/google/protobuf/stubs/bytestream.cc
+++ src/google/protobuf/stubs/bytestream.cc
@@ -124,10 +124,8 @@ char* GrowingArrayByteSink::GetBuffer(size_t* nbytes) {
 void GrowingArrayByteSink::Expand(size_t amount) {  // Expand by at least 50%.
   size_t new_capacity = std::max(capacity_ + amount, (3 * capacity_) / 2);
   char* bigger = new char[new_capacity];
-  if(buf_ != nullptr) {
-    memcpy(bigger, buf_, size_);
-    delete[] buf_;
-  }
+  memcpy(bigger, buf_, size_);
+  delete[] buf_;
   buf_ = bigger;
   capacity_ = new_capacity;
 }
