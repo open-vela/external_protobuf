a6e717400bab87fc28c42dd2209a6d1f129c348c
openvela-robot <openvela-robot@xiaomi.com>
2022-11-28 12:45:07 +0800

    Remove _internal_has_XXX APIs and directly check presence (e.g. has_bits, oneof case).
    PiperOrigin-RevId: 491417852

--- src/google/protobuf/compiler/java/name_resolver.h
+++ src/google/protobuf/compiler/java/name_resolver.h
@@ -31,13 +31,16 @@
 #ifndef GOOGLE_PROTOBUF_COMPILER_JAVA_NAME_RESOLVER_H__
 #define GOOGLE_PROTOBUF_COMPILER_JAVA_NAME_RESOLVER_H__
 
-#include <map>
 #include <string>
 
-#include <google/protobuf/stubs/common.h>
+#include "google/protobuf/stubs/logging.h"
+#include "google/protobuf/stubs/common.h"
+#include "absl/container/flat_hash_map.h"
+#include "google/protobuf/compiler/java/options.h"
+#include "google/protobuf/port.h"
 
 // Must be last.
-#include <google/protobuf/port_def.inc>
+#include "google/protobuf/port_def.inc"
 
 namespace google {
 namespace protobuf {
@@ -58,8 +61,11 @@ enum NameEquality { NO_MATCH, EXACT_EQUAL, EQUAL_IGNORE_CASE };
 // Thread-safety note: This class is *not* thread-safe.
 class ClassNameResolver {
  public:
-  ClassNameResolver();
-  ~ClassNameResolver();
+  explicit ClassNameResolver(const Options& options = {}) : options_(options) {}
+  ~ClassNameResolver() = default;
+
+  ClassNameResolver(const ClassNameResolver&) = delete;
+  ClassNameResolver& operator=(const ClassNameResolver&) = delete;
 
   // Gets the unqualified outer class name for the file.
   std::string GetFileClassName(const FileDescriptor* file, bool immutable);
@@ -135,6 +141,8 @@ class ClassNameResolver {
                                const FileDescriptor* file, bool immutable,
                                bool is_own_file, bool kotlin);
 
+  Options options_;
+
  private:
   // Get the Java Class style full name of a message.
   std::string GetJavaClassFullName(const std::string& name_without_package,
@@ -143,10 +151,8 @@ class ClassNameResolver {
                                    const FileDescriptor* file, bool immutable,
                                    bool kotlin);
   // Caches the result to provide better performance.
-  std::map<const FileDescriptor*, std::string>
+  absl::flat_hash_map<const FileDescriptor*, std::string>
       file_immutable_outer_class_names_;
-
-  GOOGLE_DISALLOW_EVIL_CONSTRUCTORS(ClassNameResolver);
 };
 
 }  // namespace java
@@ -154,6 +160,6 @@ class ClassNameResolver {
 }  // namespace protobuf
 }  // namespace google
 
-#include <google/protobuf/port_undef.inc>
+#include "google/protobuf/port_undef.inc"
 
 #endif  // GOOGLE_PROTOBUF_COMPILER_JAVA_NAME_RESOLVER_H__
