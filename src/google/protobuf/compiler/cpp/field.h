11f2b4bfb0809e32242e5e897aeaafcdb7bcead0
liujinye <liujinye@xiaomi.com>
2022-11-29 14:57:58 +0800

    Remove php 7.4 installation

--- src/google/protobuf/compiler/cpp/field.h
+++ src/google/protobuf/compiler/cpp/field.h
@@ -36,13 +36,13 @@
 #define GOOGLE_PROTOBUF_COMPILER_CPP_FIELD_H__
 
 #include <cstdint>
-#include <map>
 #include <memory>
 #include <string>
 
-#include <google/protobuf/descriptor.h>
-#include <google/protobuf/compiler/cpp/helpers.h>
-#include <google/protobuf/compiler/cpp/options.h>
+#include "google/protobuf/descriptor.h"
+#include "absl/container/flat_hash_map.h"
+#include "google/protobuf/compiler/cpp/helpers.h"
+#include "google/protobuf/compiler/cpp/options.h"
 
 namespace google {
 namespace protobuf {
@@ -57,23 +57,32 @@ namespace protobuf {
 namespace compiler {
 namespace cpp {
 
+absl::flat_hash_map<absl::string_view, std::string> FieldVars(
+    const FieldDescriptor* desc, const Options& opts);
+
+absl::flat_hash_map<absl::string_view, std::string> OneofFieldVars(
+    const FieldDescriptor* descriptor);
+
 // Helper function: set variables in the map that are the same for all
 // field code generators.
 // ['name', 'index', 'number', 'classname', 'declared_type', 'tag_size',
 // 'deprecation'].
-void SetCommonFieldVariables(const FieldDescriptor* descriptor,
-                             std::map<std::string, std::string>* variables,
-                             const Options& options);
+void SetCommonFieldVariables(
+    const FieldDescriptor* descriptor,
+    absl::flat_hash_map<absl::string_view, std::string>* variables,
+    const Options& options);
 
 void SetCommonOneofFieldVariables(
     const FieldDescriptor* descriptor,
-    std::map<std::string, std::string>* variables);
+    absl::flat_hash_map<absl::string_view, std::string>* variables);
 
 class FieldGenerator {
  public:
   explicit FieldGenerator(const FieldDescriptor* descriptor,
                           const Options& options)
       : descriptor_(descriptor), options_(options) {}
+  FieldGenerator(const FieldGenerator&) = delete;
+  FieldGenerator& operator=(const FieldGenerator&) = delete;
   virtual ~FieldGenerator();
   virtual void GenerateSerializeWithCachedSizes(
       io::Printer* printer) const final{};
@@ -150,9 +159,6 @@ class FieldGenerator {
   // method, invoked by each of the generated constructors.
   virtual void GenerateConstructorCode(io::Printer* printer) const = 0;
 
-  // Generate initialization code for private members in the cold struct.
-  virtual void GenerateCreateSplitMessageCode(io::Printer* printer) const {}
-
   // Generate any code that needs to go in the class's SharedDtor() method,
   // invoked by the destructor.
   // Most field types don't need this, so the default implementation is empty.
@@ -209,6 +215,8 @@ class FieldGenerator {
   // message fields won't need to override this function.
   virtual void GenerateIsInitialized(io::Printer* printer) const {}
 
+  virtual void GenerateIfHasField(io::Printer* printer) const;
+
   virtual bool IsInlined() const { return false; }
 
   virtual ArenaDtorNeeds NeedsArenaDestructor() const {
@@ -221,10 +229,7 @@ class FieldGenerator {
  protected:
   const FieldDescriptor* descriptor_;
   const Options& options_;
-  std::map<std::string, std::string> variables_;
-
- private:
-  GOOGLE_DISALLOW_EVIL_CONSTRUCTORS(FieldGenerator);
+  absl::flat_hash_map<absl::string_view, std::string> variables_;
 };
 
 // Convenience class which constructs FieldGenerators for a Descriptor.
@@ -232,6 +237,8 @@ class FieldGeneratorMap {
  public:
   FieldGeneratorMap(const Descriptor* descriptor, const Options& options,
                     MessageSCCAnalyzer* scc_analyzer);
+  FieldGeneratorMap(const FieldGeneratorMap&) = delete;
+  FieldGeneratorMap& operator=(const FieldGeneratorMap&) = delete;
   ~FieldGeneratorMap();
 
   const FieldGenerator& get(const FieldDescriptor* field) const;
@@ -258,8 +265,6 @@ class FieldGeneratorMap {
   static FieldGenerator* MakeGenerator(const FieldDescriptor* field,
                                        const Options& options,
                                        MessageSCCAnalyzer* scc_analyzer);
-
-  GOOGLE_DISALLOW_EVIL_CONSTRUCTORS(FieldGeneratorMap);
 };
 
 }  // namespace cpp
