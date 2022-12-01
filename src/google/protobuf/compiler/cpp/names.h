2062a5a28174a26f1f6e601d3e95dc49b8f4b9c3
liujinye <liujinye@xiaomi.com>
2022-12-01 11:38:26 +0800

    Prevent merging PRs directly in main with a failing Mergeable check, except when an `mergeable:force-allow` label is present.
    This serves to prevent accidental PR merges outside of Copybara, but is not actually enforced by branch protection.
    
    PiperOrigin-RevId: 492255443

--- src/google/protobuf/compiler/cpp/names.h
+++ src/google/protobuf/compiler/cpp/names.h
@@ -34,7 +34,7 @@
 #include <string>
 
 // Must be included last.
-#include <google/protobuf/port_def.inc>
+#include "google/protobuf/port_def.inc"
 
 namespace google {
 namespace protobuf {
@@ -43,9 +43,22 @@ class Descriptor;
 class EnumDescriptor;
 class EnumValueDescriptor;
 class FieldDescriptor;
+class FileDescriptor;
 
 namespace compiler {
 namespace cpp {
+
+// Returns the fully qualified C++ namespace.
+//
+// For example, if you had:
+//   package foo.bar;
+//   message Baz { message Moo {} }
+// Then the qualified namespace for Moo would be:
+//   ::foo::bar
+std::string Namespace(const FileDescriptor* d);
+std::string Namespace(const Descriptor* d);
+std::string Namespace(const FieldDescriptor* d);
+std::string Namespace(const EnumDescriptor* d);
 
 // Returns the unqualified C++ name.
 //
@@ -92,6 +105,6 @@ PROTOC_EXPORT std::string StripProto(const std::string& filename);
 }  // namespace protobuf
 }  // namespace google
 
-#include <google/protobuf/port_undef.inc>
+#include "google/protobuf/port_undef.inc"
 
 #endif  // GOOGLE_PROTOBUF_COMPILER_CPP_NAMES_H__
