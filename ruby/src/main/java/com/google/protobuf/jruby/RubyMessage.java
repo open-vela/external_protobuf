7afd8699d6892d06f6b791ddd6d5a93855ccec4e
openvela-robot <openvela-robot@xiaomi.com>
2023-01-31 13:56:24 +0800

    Downgrade ruby major version

--- ruby/src/main/java/com/google/protobuf/jruby/RubyMessage.java
+++ ruby/src/main/java/com/google/protobuf/jruby/RubyMessage.java
@@ -306,7 +306,11 @@ public class RubyMessage extends RubyObject {
     if (methodName.startsWith(HAS_PREFIX) && methodName.endsWith(QUESTION_MARK)) {
       String strippedMethodName = methodName.substring(4, methodName.length() - 1);
       FieldDescriptor fieldDescriptor = descriptor.findFieldByName(strippedMethodName);
-      if (fieldDescriptor != null && fieldDescriptor.hasPresence()) {
+      if (fieldDescriptor != null
+          && (!proto3
+              || fieldDescriptor.getContainingOneof() == null
+              || fieldDescriptor.getContainingOneof().isSynthetic())
+          && fieldDescriptor.hasPresence()) {
         return context.runtime.getTrue();
       }
       oneofDescriptor =
@@ -455,7 +459,11 @@ public class RubyMessage extends RubyObject {
 
         fieldDescriptor = descriptor.findFieldByName(methodName);
 
-        if (fieldDescriptor != null && fieldDescriptor.hasPresence()) {
+        if (fieldDescriptor != null
+            && (!proto3
+                || fieldDescriptor.getContainingOneof() == null
+                || fieldDescriptor.getContainingOneof().isSynthetic())
+            && fieldDescriptor.hasPresence()) {
           return fields.containsKey(fieldDescriptor) ? runtime.getTrue() : runtime.getFalse();
         }
 
