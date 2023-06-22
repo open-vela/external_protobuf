2f4a40e51cd89738816f256f0cd35369d332d199
openvela-robot <openvela-robot@xiaomi.com>
2023-06-22 09:18:12 +0800

    Put a 4k limit on preallocation to prevent OOM from malformed inputs.
    PiperOrigin-RevId: 542579832

--- src/google/protobuf/compiler/cpp/helpers.cc
+++ src/google/protobuf/compiler/cpp/helpers.cc
@@ -889,8 +889,7 @@ std::string SafeFunctionName(const Descriptor* descriptor,
 }
 
 bool IsProfileDriven(const Options& options) {
-  return !options.bootstrap && !options.opensource_runtime &&
-         options.access_info_map != nullptr;
+  return options.access_info_map != nullptr;
 }
 
 bool IsRarelyPresent(const FieldDescriptor* field, const Options& options) {
@@ -979,13 +978,6 @@ bool ShouldForceAllocationOnConstruction(const Descriptor* desc,
   (void)desc;
   (void)options;
   return false;
-}
-
-bool IsPresentMessage(const Descriptor* descriptor, const Options& options) {
-  (void)descriptor;
-  (void)options;
-  // Assume that the message is present if there is no profile.
-  return true;
 }
 
 static bool HasRepeatedFields(const Descriptor* descriptor) {
