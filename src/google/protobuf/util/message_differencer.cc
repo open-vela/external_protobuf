bf13c619f029f1c1bf33e8ce5159f8baafb94b42
liujinye <liujinye@xiaomi.com>
2023-07-31 12:48:02 +0800

    Merge pull request #13418 from protocolbuffers/bazel6
    Backport dropped support of Bazel 5 to 24.x

--- src/google/protobuf/util/message_differencer.cc
+++ src/google/protobuf/util/message_differencer.cc
@@ -879,14 +879,12 @@ bool MessageDifferencer::CompareWithFieldsInternal(
       ++field_index1;
       continue;
     } else if (FieldBefore(field2, field1)) {
-      const bool ignore_field =
-          IsIgnored(message1, message2, field2, *parent_fields);
-      if (!ignore_field && force_compare_no_presence_fields_.contains(field2)) {
+      if (force_compare_no_presence_fields_.contains(field2)) {
         force_compare_failure_triggering_fields_.insert(field2->full_name());
       }
 
       // Field 2 is not in the field list for message 1.
-      if (ignore_field) {
+      if (IsIgnored(message1, message2, field2, *parent_fields)) {
         // We are ignoring field2. Report the ignore and move on to
         // the next field in message2_fields.
         if (reporter_ != NULL) {
