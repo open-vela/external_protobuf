fff5984910af147b064564bc7a384fe4997ab463
liujinye <liujinye@xiaomi.com>
2023-08-09 05:33:28 +0800

    Support without-presence scalars better.
    Only emit has_field() if the field support presence. Only emit field_opt() getter if the field is both optional and supports presence.
    
    PiperOrigin-RevId: 555133374

--- src/google/protobuf/extension_set.h
+++ src/google/protobuf/extension_set.h
@@ -49,6 +49,7 @@
 #include "absl/base/call_once.h"
 #include "absl/container/btree_map.h"
 #include "absl/log/absl_check.h"
+#include "google/protobuf/internal_visibility.h"
 #include "google/protobuf/port.h"
 #include "google/protobuf/port.h"
 #include "google/protobuf/io/coded_stream.h"
@@ -76,21 +77,16 @@ class Message;          // message.h
 class MessageFactory;   // message.h
 class Reflection;       // message.h
 class UnknownFieldSet;  // unknown_field_set.h
-#ifdef PROTOBUF_FUTURE_EDITIONS
 class FeatureSet;
-#endif  // PROTOBUF_FUTURE_EDITIONS
 namespace internal {
 class FieldSkipper;  // wire_format_lite.h
 class WireFormat;
-enum class LazyVerifyOption;
 }  // namespace internal
 }  // namespace protobuf
 }  // namespace google
-#ifdef PROTOBUF_FUTURE_EDITIONS
 namespace pb {
 class CppFeatures;
 }  // namespace pb
-#endif  // PROTOBUF_FUTURE_EDITIONS
 
 namespace google {
 namespace protobuf {
@@ -194,10 +190,18 @@ class PROTOBUF_EXPORT GeneratedExtensionFinder {
 // off to the ExtensionSet for parsing.  Etc.
 class PROTOBUF_EXPORT ExtensionSet {
  public:
-  constexpr ExtensionSet();
-  explicit ExtensionSet(Arena* arena);
+  constexpr ExtensionSet() : ExtensionSet(nullptr) {}
+  ExtensionSet(const ExtensionSet& rhs) = delete;
+
+  // Arena enabled constructors: for internal use only.
+  ExtensionSet(internal::InternalVisibility, Arena* arena)
+      : ExtensionSet(arena) {}
+
+  // TODO(b/290091828): make constructor private, and migrate `ArenaInitialized`
+  // to `InternalVisibility` overloaded constructor(s).
+  explicit constexpr ExtensionSet(Arena* arena);
   ExtensionSet(ArenaInitialized, Arena* arena) : ExtensionSet(arena) {}
-  ExtensionSet(const ExtensionSet&) = delete;
+
   ExtensionSet& operator=(const ExtensionSet&) = delete;
   ~ExtensionSet();
 
@@ -589,8 +593,8 @@ class PROTOBUF_EXPORT ExtensionSet {
     virtual void Clear() = 0;
 
     virtual const char* _InternalParse(const MessageLite& prototype,
-                                       Arena* arena, LazyVerifyOption option,
-                                       const char* ptr, ParseContext* ctx) = 0;
+                                       Arena* arena, const char* ptr,
+                                       ParseContext* ctx) = 0;
     virtual uint8_t* WriteMessageToArray(
         const MessageLite* prototype, int number, uint8_t* target,
         io::EpsCopyOutputStream* stream) const = 0;
@@ -932,8 +936,8 @@ class PROTOBUF_EXPORT ExtensionSet {
   static void DeleteFlatMap(const KeyValue* flat, uint16_t flat_capacity);
 };
 
-constexpr ExtensionSet::ExtensionSet()
-    : arena_(nullptr), flat_capacity_(0), flat_size_(0), map_{nullptr} {}
+constexpr ExtensionSet::ExtensionSet(Arena* arena)
+    : arena_(arena), flat_capacity_(0), flat_size_(0), map_{nullptr} {}
 
 // These are just for convenience...
 inline void ExtensionSet::SetString(int number, FieldType type,
@@ -1545,7 +1549,6 @@ class ExtensionIdentifier {
 extern PROTOBUF_ATTRIBUTE_WEAK ExtensionSet::LazyMessageExtension*
 MaybeCreateLazyExtension(Arena* arena);
 
-#ifdef PROTOBUF_FUTURE_EDITIONS
 // Define a specialization of ExtensionIdentifier for bootstrapped extensions
 // that we need to register lazily.
 template <>
@@ -1577,7 +1580,6 @@ class ExtensionIdentifier<FeatureSet, MessageTypeTraits<::pb::CppFeatures>, 11,
   mutable absl::once_flag once_;
 };
 
-#endif  // PROTOBUF_FUTURE_EDITIONS
 
 }  // namespace internal
 
--- src/google/protobuf/implicit_weak_message.h
+++ src/google/protobuf/implicit_weak_message.h
@@ -57,13 +57,19 @@ namespace internal {
 // message type does not get linked into the binary.
 class PROTOBUF_EXPORT ImplicitWeakMessage : public MessageLite {
  public:
-  ImplicitWeakMessage() : data_(new std::string) {}
+  ImplicitWeakMessage() : ImplicitWeakMessage(nullptr) {}
   explicit constexpr ImplicitWeakMessage(ConstantInitialized)
       : data_(nullptr) {}
-  explicit ImplicitWeakMessage(Arena* arena)
-      : MessageLite(arena), data_(new std::string) {}
   ImplicitWeakMessage(const ImplicitWeakMessage&) = delete;
   ImplicitWeakMessage& operator=(const ImplicitWeakMessage&) = delete;
+
+  // Arena enabled constructors: for internal use only.
+  ImplicitWeakMessage(internal::InternalVisibility, Arena* arena)
+      : ImplicitWeakMessage(arena) {}
+
+  // TODO(b/290091828): make this constructor private
+  explicit ImplicitWeakMessage(Arena* arena)
+      : MessageLite(arena), data_(new std::string) {}
 
   ~ImplicitWeakMessage() override {
     // data_ will be null in the default instance, but we can safely call delete
@@ -156,8 +162,21 @@ struct WeakRepeatedPtrField {
   using DestructorSkippable_ = void;
 
   using TypeHandler = internal::ImplicitWeakTypeHandler<T>;
+
   constexpr WeakRepeatedPtrField() : weak() {}
+  WeakRepeatedPtrField(const WeakRepeatedPtrField& rhs)
+      : WeakRepeatedPtrField(nullptr, rhs) {}
+
+  // Arena enabled constructors: for internal use only.
+  WeakRepeatedPtrField(internal::InternalVisibility, Arena* arena)
+      : WeakRepeatedPtrField(arena) {}
+  WeakRepeatedPtrField(internal::InternalVisibility, Arena* arena,
+                       const WeakRepeatedPtrField& rhs)
+      : WeakRepeatedPtrField(arena, rhs) {}
+
+  // TODO(b/290091828): make this constructor private
   explicit WeakRepeatedPtrField(Arena* arena) : weak(arena) {}
+
   ~WeakRepeatedPtrField() { weak.template Destroy<TypeHandler>(); }
 
   typedef internal::RepeatedPtrIterator<MessageLite> iterator;
@@ -179,13 +198,13 @@ struct WeakRepeatedPtrField {
     return pointer_iterator(base().raw_mutable_data());
   }
   const_pointer_iterator pointer_begin() const {
-    return const_pointer_iterator(base().raw_mutable_data());
+    return const_pointer_iterator(base().raw_data());
   }
   pointer_iterator pointer_end() {
     return pointer_iterator(base().raw_mutable_data() + base().size());
   }
   const_pointer_iterator pointer_end() const {
-    return const_pointer_iterator(base().raw_mutable_data() + base().size());
+    return const_pointer_iterator(base().raw_data() + base().size());
   }
 
   MessageLite* AddWeak(const MessageLite* prototype) {
@@ -208,6 +227,12 @@ struct WeakRepeatedPtrField {
   union {
     RepeatedPtrField<T> weak;
   };
+
+ private:
+  WeakRepeatedPtrField(Arena* arena, const WeakRepeatedPtrField& rhs)
+      : WeakRepeatedPtrField(arena) {
+    MergeFrom(rhs);
+  }
 };
 
 }  // namespace protobuf
