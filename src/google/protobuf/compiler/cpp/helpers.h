7bce8a0de790cb3a5016c5e21de29a74e67419cf
openvela-robot <openvela-robot@xiaomi.com>
2023-10-17 09:11:39 +0800

    Set `PROTOBUF_EXPORT` on `InternalOutOfLineDeleteMessageLite()` (#14434)
    This function needs to be visible to generated code, so this CL ensures that
    it's exported on libprotobuf.so.
    
    PiperOrigin-RevId: 573980166

--- src/google/protobuf/compiler/cpp/helpers.h
+++ src/google/protobuf/compiler/cpp/helpers.h
@@ -331,7 +331,7 @@ inline bool IsCord(const FieldDescriptor* field) {
          internal::cpp::EffectiveStringCType(field) == FieldOptions::CORD;
 }
 
-inline bool IsString(const FieldDescriptor* field) {
+inline bool IsString(const FieldDescriptor* field, const Options& options) {
   return field->cpp_type() == FieldDescriptor::CPPTYPE_STRING &&
          internal::cpp::EffectiveStringCType(field) == FieldOptions::STRING;
 }
@@ -354,9 +354,6 @@ float GetPresenceProbability(const FieldDescriptor* field,
                              const Options& options);
 
 bool IsStringInliningEnabled(const Options& options);
-
-// Returns true if the provided field is a singular string and can be inlined.
-bool CanStringBeInlined(const FieldDescriptor* field);
 
 // Returns true if `field` should be inlined based on PDProto profile.
 bool IsStringInlined(const FieldDescriptor* field, const Options& options);
@@ -532,8 +529,8 @@ inline std::string MakeVarintCachedSizeFieldName(const FieldDescriptor* field,
 // while the two functions below use FileDescriptor::name(). In a sane world the
 // two approaches should be equivalent. But if you are dealing with descriptors
 // from untrusted sources, you might need to match semantics across libraries.
-bool IsAnyMessage(const FileDescriptor* descriptor);
-bool IsAnyMessage(const Descriptor* descriptor);
+bool IsAnyMessage(const FileDescriptor* descriptor, const Options& options);
+bool IsAnyMessage(const Descriptor* descriptor, const Options& options);
 
 bool IsWellKnownMessage(const FileDescriptor* descriptor);
 
